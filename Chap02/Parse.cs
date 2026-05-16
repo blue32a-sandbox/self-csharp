d051ba4426b712db2acc2dd61deceab655fc3abb
blue32a <blue32a.jp@gmail.com>
2026-05-16 17:28:54 +0900

    リスト2.5 文字列から数値の変換

---
+++ Chap02/Parse.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap02
+{
+    internal class Parse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Int32.Parse("108")); // int 108
+            Console.WriteLine(Single.Parse("0.1413")); // float 0.1413
+            Console.WriteLine(Int32.Parse("FF", System.Globalization.NumberStyles.HexNumber)); // int 255
+            Console.WriteLine(Double.Parse("0.653e2")); // double 65.3
+            Console.WriteLine(Convert.ToInt32("1010", 2)); // int 10
+        }
+    }
+}
