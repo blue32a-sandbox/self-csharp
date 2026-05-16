f1ac178b94c6615c9a4fa145fa0f4c7976d9fb2f
blue32a <blue32a.jp@gmail.com>
2026-05-16 18:02:40 +0900

    リスト2.7 new式の型名を省略

---
+++ Chap02/FieldNew.cs
@@ -0,0 +1,16 @@
+﻿namespace SelfCSharp.Chap02
+{
+    internal class FieldNew
+    {
+        // public SrtedDictionary<string, FileInfo> Map = new SortedDictionary<string, FileInfo>();
+        public SortedDictionary<string, FileInfo> Map = new();
+
+        static void Main(string[] args)
+        {
+            var dt = new DateTime(2022, 02, 15, 13, 17, 23);
+            Console.WriteLine(dt); // 2022/02/15 13:17:23
+            // new TimeSpan(3, 15, 30, 0)
+            Console.WriteLine(dt.Add(new(3, 15, 30, 0))); // 2022/02/19 4:47:23
+        }
+    }
+}
