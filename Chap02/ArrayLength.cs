592d44e84074eff6ea4f38a8c5bf1e45733334ea
blue32a <blue32a.jp@gmail.com>
2026-05-16 20:47:18 +0900

    リスト2.14,2.15 配列サイズと次元数

---
+++ Chap02/ArrayLength.cs
@@ -0,0 +1,25 @@
+﻿namespace SelfCSharp.Chap02
+{
+    internal class ArrayLength
+    {
+        static void Main(string[] args)
+        {
+            // 多次元配列のサイズと次元数
+            var multi = new[,] {
+                { 10, 11, 12 },
+                { 20, 21, 22 },
+            };
+            Console.WriteLine(multi.Length); // 6
+            Console.WriteLine(multi.Rank); // 2
+
+            // ジャグ配列のサイズと次元数
+            var jagged = new int[3][];
+            jagged[0] = new[] { 10, 11, 12, 13 };
+            jagged[1] = new[] { 20, 21 };
+            jagged[2] = new[] { 30, 31, 32 };
+            Console.WriteLine(jagged.Length); // 3
+            Console.WriteLine(jagged[0].Length); // 4
+            Console.WriteLine(jagged.Rank); // 1
+        }
+    }
+}
