8237166c2103fc78269041747c44e9768b0652af
blue32a <blue32a.jp@gmail.com>
2026-05-16 18:19:26 +0900

    リスト2.8 null許容型との計算

---
+++ Chap02/NullableCalc.cs
@@ -0,0 +1,13 @@
+﻿namespace SelfCSharp.Chap02
+{
+    internal class NullableCalc
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = 3;
+            int num2 = 5;
+            // num1.Value + num2
+            Console.WriteLine(num1 + num2); // 8
+        }
+    }
+}
