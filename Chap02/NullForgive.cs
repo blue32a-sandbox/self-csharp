3ef03dcb8d595b271b33929f111d23b1fb2fc832
blue32a <blue32a.jp@gmail.com>
2026-05-16 18:36:52 +0900

    リスト2.10 null免除演算子

---
+++ Chap02/NullForgive.cs
@@ -0,0 +1,22 @@
+﻿namespace SelfCSharp.Chap02
+{
+    internal class NullForgive
+    {
+        static void Main(string[] args)
+        {
+            string?[] format = { "2022-12-25 10:16:23" };
+            // nullである可能性があるので警告が発生する
+            //var dt = DateTime.Parse(format[0]);
+
+            if (format[0] is {  } item) // nullチェック
+            {
+                var dt1 = DateTime.Parse(item);
+                Console.WriteLine(dt1);
+            }
+
+            // null免除演算子 !
+            var dt2 = DateTime.Parse(format[0]!);
+            Console.WriteLine(dt2);
+        }
+    }
+}
