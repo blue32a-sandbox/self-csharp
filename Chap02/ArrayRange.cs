015d3e2fd9a854f2b68e220b147a28729fc5c9da
blue32a <blue32a.jp@gmail.com>
2026-05-16 20:34:57 +0900

    リスト2.11 配列の範囲指定

---
+++ Chap02/ArrayRange.cs
@@ -0,0 +1,16 @@
+﻿namespace SelfCSharp.Chap02
+{
+    internal class ArrayRange
+    {
+        static void Main(string[] args)
+        {
+            var list = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine(string.Join(" ", list[1..3])); // 2 3
+            Console.WriteLine(string.Join(" ", list[..2])); // 1 2
+            Console.WriteLine(string.Join(" ", list[2..])); // 3 4 5 6 7 8 9
+            Console.WriteLine(string.Join(" ", list[^2])); // 8
+            Console.WriteLine(string.Join(" ", list[1..^1])); // 2 3 4 5 6 7 8
+            Console.WriteLine(string.Join(" ", list[1..1])); // (empty)
+        }
+    }
+}
