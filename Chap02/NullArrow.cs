880649d02747bd0f37a681cefd6592df2f644bec
blue32a <blue32a.jp@gmail.com>
2026-05-16 18:27:05 +0900

    リスト2.9 null値禁止

---
+++ Chap02/NullArrow.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap02
+{
+    internal class NullArrow
+    {
+        static void Main(string[] args)
+        {
+            // プロジェクトでnull許容を有効化している状態だと、警告が発生する
+            string str = null;
+            // string str = "";
+            // string? str = null;
+            Console.WriteLine(str);
+        }
+    }
+}
