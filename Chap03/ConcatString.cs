ef5569ad2ff5ad7a727a8b309d68ab83757eed23
blue32a <blue32a.jp@gmail.com>
2026-05-17 21:30:07 +0900

    リスト3.2 +演算子による文字列連結

---
+++ Chap03/ConcatString.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class ConcatString
+    {
+        static void Main(string[] args)
+        {
+            var result = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                result += "いろは";
+            }
+        }
+    }
+}
