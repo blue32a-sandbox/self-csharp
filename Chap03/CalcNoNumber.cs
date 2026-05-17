c51a6e9a28f027fc7fe9b00448e7142ac782c655
blue32a <blue32a.jp@gmail.com>
2026-05-17 21:25:11 +0900

    リスト3.1 非数値が混在する演算

---
+++ Chap03/CalcNoNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class CalcNoNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1 + 2); // 3
+            Console.WriteLine("a" + 5); // a5
+            Console.WriteLine(5 + "b"); // 5b
+            Console.WriteLine("1" + "2"); // 12
+            Console.WriteLine("a" + DateTime.Now); // aYYYY/MM/DD HH:MM:SS
+        }
+    }
+}
