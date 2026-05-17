c2fb7f1ee624e07b760aa7364463917b4b80c7da
blue32a <blue32a.jp@gmail.com>
2026-05-17 21:51:47 +0900

    リスト3.4 インクリメント演算子の前置／後置演算

---
+++ Chap03/Increment.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class Increment
+    {
+        static void Main(string[] args)
+        {
+            var i = 3;
+            var j = ++i;
+            Console.WriteLine(i); // 4
+            Console.WriteLine(j); // 4
+
+            var m = 3;
+            var n = m++;
+            Console.WriteLine(m); // 4
+            Console.WriteLine(n); // 3
+        }
+    }
+}
