f9fdb44c0f2ce873a3a56c8fb5d680c836243389
blue32a <blue32a.jp@gmail.com>
2026-05-17 21:55:51 +0900

    リスト3.5 ゼロ除算

---
+++ Chap03/CalcDivZero.cs
@@ -0,0 +1,13 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class CalcDivZero
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(3 / 0); // エラー：定数 0 による除算です
+            //Console.WriteLine(3 % 0); // エラー：定数 0 による除算です
+            Console.WriteLine(3d / 0); // ∞
+            Console.WriteLine(3d % 0); // NaN
+        }
+    }
+}
