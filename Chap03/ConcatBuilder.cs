dad9a32813908a59bb047e81e8b50beb6f76ffdf
blue32a <blue32a.jp@gmail.com>
2026-05-17 21:47:35 +0900

    リスト3.3 StringBuilderによる文字列連結

---
+++ Chap03/ConcatBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace SelfCSharp.Chap03
+{
+    internal class ConcatBuilder
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            for (int i = 0; i < 100000; i++)
+            {
+                builder.Append("いろは");
+            }
+            var result = builder.ToString();
+        }
+    }
+}
