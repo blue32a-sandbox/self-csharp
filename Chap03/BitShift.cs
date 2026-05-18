cb274f41ce1828bedbb117a8cdda2788c91cf94a
blue32a <blue32a.jp@gmail.com>
2026-05-18 21:41:27 +0900

    リスト3.15 算術シフトと論理シフト

---
+++ Chap03/BitShift.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class BitShift
+    {
+        static void Main(string[] args)
+        {
+            // int型の最小値を設定
+            int i = int.MinValue;
+            Console.WriteLine($"{Convert.ToString(i, 2)}");
+            Console.WriteLine($"{Convert.ToString(i >> 5, 2)}");
+
+            // int型の最小値をuint型にキャスト
+            uint m = (uint)i;
+            Console.WriteLine($"{Convert.ToString(m, 2)}");
+            Console.WriteLine($"{Convert.ToString(m >> 5, 2), 32}");
+        }
+    }
+}
