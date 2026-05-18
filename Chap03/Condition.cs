7758c9d93f79b7961d76cfaecf524121fa09f7c9
blue32a <blue32a.jp@gmail.com>
2026-05-18 21:26:15 +0900

    リスト3.12 条件演算子（三項演算子）

---
+++ Chap03/Condition.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class Condition
+    {
+        static void Main(string[] args)
+        {
+            var score = 75;
+            Console.WriteLine(score >= 70 ? "合格！" : "不合格..."); // 合格！
+            
+            bool flag = true;
+            // flag ? Console.WriteLine("OK") : Console.WriteLine("NG");
+            Console.WriteLine(flag ? "OK" : "NG"); // OK
+
+            Console.WriteLine($"{( true ? "OK" : "NG")}"); // OK
+        }
+    }
+}
