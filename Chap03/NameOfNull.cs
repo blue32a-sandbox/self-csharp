ba0135367402c7c548c59436af7b1d8f84962cbd
blue32a <blue32a.jp@gmail.com>
2026-05-18 21:51:02 +0900

    リスト3.18 nameof演算子で識別子を文字列として扱う

---
+++ Chap03/NameOfNull.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class NameOfNull
+    {
+        static void Main(string[] args)
+        {
+            new NameOfNull().Hoge(null);
+        }
+
+        public void Hoge(string? str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
