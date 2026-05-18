0c69b5127aa8b48674a786d1cf8c3bf8af939d3d
blue32a <blue32a.jp@gmail.com>
2026-05-18 21:36:50 +0900

    リスト3.14 ショートカット演算子

---
+++ Chap03/Shortcut.cs
@@ -0,0 +1,20 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class Shortcut
+    {
+        static void Main(string[] args)
+        {
+            string? str = null;
+            if(str != null && str.StartsWith("https://"))
+            {
+                Console.WriteLine("「https://～」で始まります。");
+            }
+
+            // &演算子はショートカットの性質を持たない
+            //if (str != null & str.StartsWith("https://"))
+            //{
+            //    Console.WriteLine("「https://～」で始まります。");
+            //}
+        }
+    }
+}
