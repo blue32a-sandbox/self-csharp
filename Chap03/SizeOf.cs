e27f30469356073c261172f8075d7a00deb87a9c
blue32a <blue32a.jp@gmail.com>
2026-05-18 21:44:33 +0900

    リスト3.16 sizeof演算子

---
+++ Chap03/SizeOf.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap03
+{
+    internal class SizeOf
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sizeof(int)); // 4
+            Console.WriteLine(sizeof(decimal)); // 16
+
+            // unsafeモードを利用することで、参照型を含まない構造体やポインターのサイズを取得できる
+            //unsafe
+            //{
+            //    Console.WriteLine(sizeof(MyStruct));
+            //}
+        }
+    }
+}
