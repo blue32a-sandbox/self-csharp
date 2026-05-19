8a525a6f083a79e38747eb5c5a8152002e320ceb
blue32a <blue32a.jp@gmail.com>
2026-05-19 23:14:22 +0900

    リスト4.35 プリプロセッサディレクティブ #region

---
+++ Chap04/PreRegion.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap04
+{
+    internal class PreRegion
+    {
+        static void Main(string[] args)
+        {
+            #region
+            const string Publisher = "翔泳社";
+            const double Tax = 1.1;
+            var author = "WINGSプロジェクト";
+            var title = "１人で学ぶC#";
+            var price = 1000;
+            #endregion
+
+            Console.WriteLine($"{title} ({Publisher}刊、{author}著) 発売開始");
+            Console.WriteLine($"{price * Tax}円");
+        }
+    }
+}
