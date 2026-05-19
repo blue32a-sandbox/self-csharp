4ef77ef58564a410b71c483d48f00c5b084b9880
blue32a <blue32a.jp@gmail.com>
2026-05-19 23:19:40 +0900

    リスト5.2 サロゲートペアを含む文字列を正しくカウントする

---
+++ Chap05/StrInfoLength.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace SelfCSharp.Chap05
+{
+    internal class StrInfoLength
+    {
+        static void Main(string[] args)
+        {
+            var str = "叱る";
+            var strInfo = new StringInfo(str);
+            Console.WriteLine(strInfo.LengthInTextElements); // 2
+        }
+    }
+}
