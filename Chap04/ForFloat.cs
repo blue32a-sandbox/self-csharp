3852aa58aab4aaa1b61f7086093c6414cb5683e2
blue32a <blue32a.jp@gmail.com>
2026-05-19 22:22:57 +0900

    リスト4.22 カウンター変数に浮動小数点数を利用しない

---
+++ Chap04/ForFloat.cs
@@ -0,0 +1,15 @@
+﻿namespace SelfCSharp.Chap04
+{
+    internal class ForFloat
+    {
+        static void Main(string[] args)
+        {
+            // 誤差が発生し、ループは９回で終了してしまう
+            for (var i = 0.1f; i <= 1.0; i += 0.1f)
+            {
+                // 出力した変数iの値も正しくない
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
