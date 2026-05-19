87c72cf9a48fb947d5147b480138862452fbd969
blue32a <blue32a.jp@gmail.com>
2026-05-19 23:18:04 +0900

    リスト5.1 文字列の長さを取得する Lengthプロパティ

---
+++ Chap05/StrLength.cs
@@ -0,0 +1,15 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class StrLength
+    {
+        static void Main(string[] args)
+        {
+            var str1 = "WINGSプロジェクト";
+            Console.WriteLine(str1.Length); // 11
+
+            // サロゲートペアを含む文字列
+            var str2 = "叱る";
+            Console.WriteLine(str2.Length); // 3
+        }
+    }
+}
