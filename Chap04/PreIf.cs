7d97361fa186668e7a128b65b6ad8dcedd4e729e
blue32a <blue32a.jp@gmail.com>
2026-05-19 23:10:11 +0900

    リスト4.34 プリプロセッサディレクティブ #if

---
+++ Chap04/PreIf.cs
@@ -0,0 +1,16 @@
+﻿#define DEBUG
+
+namespace SelfCSharp.Chap04
+{
+    internal class PreIf
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("この行は常に表示されます。");
+            #if DEBUG
+            Console.WriteLine("デバッグ時にだけ表示します。");
+            #endif
+            Console.WriteLine("この行は常に表示されます。");
+        }
+    }
+}
