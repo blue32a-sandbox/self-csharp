49bda153b64b5419df525bd94d456d3de5b7c411
blue32a <blue32a.jp@gmail.com>
2026-05-19 22:29:10 +0900

    リスト4.22 ブロック配下でカウンター変数を操作しない

---
+++ Chap04/ForBlock.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap04
+{
+    internal class ForBlock
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    // ブロック配下でカウンター変数を操作しない
+                    i++;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
