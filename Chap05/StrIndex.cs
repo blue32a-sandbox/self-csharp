41a76de1bb8892240d917f9a7fe336b093ef618a
blue32a <blue32a.jp@gmail.com>
2026-05-20 21:07:43 +0900

    リスト5.7 文字列を検索する

---
+++ Chap05/StrIndex.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class StrIndex
+    {
+        static void Main(string[] args)
+        {
+            var str = "にわにはにわにわとりがいる";
+            Console.WriteLine(str.IndexOf("にわ")); // 0
+            Console.WriteLine(str.IndexOf("にも")); // -1
+            Console.WriteLine(str.LastIndexOf("にわ")); // 6
+            Console.WriteLine(str.IndexOf("にわ", 3)); // 4
+            Console.WriteLine(str.LastIndexOf("にわ", 3)); // 0
+            Console.WriteLine(str.IndexOf("にわ", 2, 5)); // 4
+            Console.WriteLine(str.LastIndexOf("にわ", 5, 3)); // 4
+            Console.WriteLine(str.IndexOf("にわ", 5, 10)); // エラー
+        }
+    }
+}
