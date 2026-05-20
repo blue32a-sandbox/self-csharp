f04416cb549b52302e649d34a6a326ad412f751e
blue32a <blue32a.jp@gmail.com>
2026-05-20 21:10:38 +0900

    リスト5.8 文字列に特定の文字列が含まれるかを判定する

---
+++ Chap05/StrContains.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class StrContains
+    {
+        static void Main(string[] args)
+        {
+            var str = "WINGSプロジェクト";
+
+            Console.WriteLine(str.Contains("プロ")); // True
+            Console.WriteLine(str.StartsWith("WINGS")); // True
+            Console.WriteLine(str.EndsWith("WINGS")); // False
+        }
+    }
+}
