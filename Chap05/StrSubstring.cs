1823a0da39e26a306fdae3947439a28af0f77b68
blue32a <blue32a.jp@gmail.com>
2026-05-20 21:19:41 +0900

    リスト5.11 部分文字列を取得する

---
+++ Chap05/StrSubstring.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class StrSubstring
+    {
+        static void Main(string[] args)
+        {
+            var str = "WINGSプロジェクト";
+            Console.WriteLine(str.Substring(5, 2)); // プロ
+
+            var path = @"C:\data\wings.jpg";
+            Console.WriteLine(path.Substring(path.LastIndexOf(".") + 1)); // jpg
+        }
+    }
+}
