4a7bb528e6b609e5a4412d70117b8c0450431b8b
blue32a <blue32a.jp@gmail.com>
2026-05-21 23:31:22 +0900

    リスト5.31 Unicodeカテゴリ／ブロックで検索する

---
+++ Chap05/RegUnicode.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SelfCSharp.Chap05
+{
+    internal class RegUnicode
+    {
+        static void ShowMatch(string msg, Regex reg)
+        {
+            foreach (Match result in reg.Matches(msg))
+            {
+                Console.WriteLine(result.Value);
+            }
+            Console.WriteLine("----------------------------");
+        }
+
+        static void Main(string[] args)
+        {
+            var msg = "ただいま、WINGSプロジェクトメンバー募集中です！";
+            ShowMatch(msg, new Regex(@"\p{IsHiragana}+")); // ただいま、です
+            ShowMatch(msg, new Regex(@"\p{IsKatakana}+")); // プロジェクトメンバー
+            ShowMatch(msg, new Regex(@"\p{IsCJKUnifiedIdeographs}+")); // 募集中
+        }
+    }
+}
