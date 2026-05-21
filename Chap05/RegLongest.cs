5510aab799e8af8ab0bec39d2f1423ff60c2b619
blue32a <blue32a.jp@gmail.com>
2026-05-21 22:34:55 +0900

    リスト5.25 最短一致で検索する

---
+++ Chap05/RegLongest.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SelfCSharp.Chap05
+{
+    internal class RegLongest
+    {
+        static void Main(string[] args)
+        {
+            var tags = "<p><string>WINGS</strong>サイト<a href='index.html'><img src='wings.jpg'></img></a></p>";
+            var rgx = new Regex(@"<.+?>");
+            var result = rgx.Matches(tags);
+
+            foreach (Match m in result)
+            {
+                Console.WriteLine(m.Value);
+            }
+        }
+    }
+}
