84c261cc032ad579c10cdcd4f8f80f8e623b83a7
blue32a <blue32a.jp@gmail.com>
2026-05-21 22:31:07 +0900

    リスト5.24 正規表現オプションを埋め込みフラグとして記述する

---
+++ Chap05/RegInline.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SelfCSharp.Chap05
+{
+    internal class RegInline
+    {
+        static void Main(string[] args)
+        {
+            var str = "仕事用はwings@example.comです。プライベート用はYAMA@example.comです。";
+
+            var rgx1 = new Regex(@"([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\.[a-z0-9-]+)*)", RegexOptions.IgnoreCase);
+            foreach (Match m in rgx1.Matches(str))
+            {
+                Console.WriteLine(m.Value);
+            }
+
+            var rgx2 = new Regex(@"(?i)([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\.[a-z0-9-]+)*)");
+            foreach (Match m in rgx2.Matches(str))
+            {
+                Console.WriteLine(m.Value);
+            }
+
+            var rgx3 = new Regex(@"(?i:([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\.[a-z0-9-]+)*))");
+            foreach (Match m in rgx3.Matches(str))
+            {
+                Console.WriteLine(m.Value);
+            }
+        }
+    }
+}
