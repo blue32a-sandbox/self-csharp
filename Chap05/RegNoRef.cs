5ba317d884c3383951ea1f6db61a7a01f22ba290
blue32a <blue32a.jp@gmail.com>
2026-05-21 22:57:08 +0900

    リスト5.29 参照しないグループをサブマッチの対象から除外する

---
+++ Chap05/RegNoRef.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SelfCSharp.Chap05
+{
+    internal class RegNoRef
+    {
+        static void Main(string[] args)
+        {
+            var msg = "仕事用はwings@example.comです。プライベート用はYAMA@example.comです。";
+            var rgx = new Regex(@"([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(?:\.[a-z0-9-]+)*)", RegexOptions.IgnoreCase);
+            foreach (Match m in rgx.Matches(msg))
+            {
+                foreach (Group sub in m.Groups)
+                {
+                    Console.WriteLine(sub.Value);
+                }
+            }
+        }
+    }
+}
