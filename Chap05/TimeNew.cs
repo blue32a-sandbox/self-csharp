3ca73e66fb5db3a596c2703643ae55262f35d7fb
blue32a <blue32a.jp@gmail.com>
2026-05-22 23:29:11 +0900

    リスト5.38 指定された年月日時分秒からオブジェクトを生成する

---
+++ Chap05/TimeNew.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class TimeNew
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new DateTime(2026, 05, 22, 23, 27, 17));
+        }
+    }
+}
