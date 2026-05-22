8611f3ee40c9b9f22fa1e58bd252d6ec0fcb1bda
blue32a <blue32a.jp@gmail.com>
2026-05-23 00:31:12 +0900

    リスト5.47 加算／減算する日時をTimeSpanオブジェクトで表す

---
+++ Chap05/TimeSpanAdd.cs
@@ -0,0 +1,12 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class TimeSpanAdd
+    {
+        static void Main(string[] args)
+        {
+            var dt = new DateTime(2026, 05, 23, 00, 28, 05);
+            var span = new TimeSpan(3, 15, 30, 0);
+            Console.WriteLine(dt.Add(span)); // 2026/05/26 15:58:05
+        }
+    }
+}
