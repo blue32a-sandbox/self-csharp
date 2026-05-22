42a54e298e16a8a91432d70eb683326befe9217c
blue32a <blue32a.jp@gmail.com>
2026-05-22 23:50:15 +0900

    リスト5.40 日付／時刻文字列からの変換の成否をbool値で返す

---
+++ Chap05/TimeParseTry.cs
@@ -0,0 +1,23 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class TimeParseTry
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt;
+            if (DateTime.TryParse("2026/05/22 23:40:17", out dt))
+            {
+                Console.WriteLine(dt);
+            }
+
+            if (DateTime.TryParse("20260522234017", out dt))
+            {
+                Console.WriteLine(dt);
+            }
+            else
+            {
+                Console.WriteLine("変換できませんでした。");
+            }
+        }
+    }
+}
