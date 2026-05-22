267c238d7f4aed5c4651ca8bccb4b1a38654016e
blue32a <blue32a.jp@gmail.com>
2026-05-23 00:24:37 +0900

    リスト5.45 和暦を表示する

---
+++ Chap05/TimeJapaniese.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace SelfCSharp.Chap05
+{
+    internal class TimeJapaniese
+    {
+        static void Main(string[] args)
+        {
+            var dt = new DateTime(2026, 05, 23, 01, 21, 05);
+            var cal = new CultureInfo("ja-JP");
+            cal.DateTimeFormat.Calendar = new JapaneseCalendar();
+            Console.WriteLine(dt.ToString("ggyy年MM月dd日 (ddd) tt hh:mm:ss", cal));
+        }
+    }
+}
