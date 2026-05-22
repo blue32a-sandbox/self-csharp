9f864104ee3c69f4d4dff500844b4b42cf51bfeb
blue32a <blue32a.jp@gmail.com>
2026-05-22 23:37:25 +0900

    リスト5.39 日付／時刻文字列から変換する

---
+++ Chap05/TimeParse.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace SelfCSharp.Chap05
+{
+    internal class TimeParse
+    {
+        static void Main(string[] args)
+        {
+            var dt1 = "2026/05/22 23:32:17";
+            Console.WriteLine(DateTime.Parse(dt1));
+
+            var dt2 = "令和8年5月22日 23時32分17秒";
+            Console.WriteLine(DateTime.Parse(dt2));
+
+            var dt3 = "Donnerstag, 22. Mai 2026 23:32:17";
+            Console.WriteLine(DateTime.Parse(dt3, new CultureInfo("de-DE")));
+        }
+    }
+}
