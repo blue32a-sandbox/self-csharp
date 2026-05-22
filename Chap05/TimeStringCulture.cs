3b38832439b67ca1a056cdfacf35f272d3727c0b
blue32a <blue32a.jp@gmail.com>
2026-05-23 00:16:30 +0900

    リスト5.44 ロケール情報に応じたフォーマット

---
+++ Chap05/TimeStringCulture.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace SelfCSharp.Chap05
+{
+    internal class TimeStringCulture
+    {
+        static void Main(string[] args)
+        {
+            var dt = new DateTime(2026, 05, 23, 00, 01, 24);
+            var culture = new CultureInfo("ja-JP");
+
+            Console.WriteLine(dt.ToString("f", culture));
+            Console.WriteLine(dt.ToString("yy/MM/dd dddd tt hh:mm:ss", culture));
+        }
+    }
+}
