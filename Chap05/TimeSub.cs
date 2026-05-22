3c46d2c135839d777d90facb6347fa00dabfde7b
blue32a <blue32a.jp@gmail.com>
2026-05-23 00:35:06 +0900

    リスト5.48 日付／時刻値の差分を取得する

---
+++ Chap05/TimeSub.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class TimeSub
+    {
+        static void Main(string[] args)
+        {
+            var dt1 = new DateTime(2026, 05, 23, 00, 32, 05);
+            var dt2 = new DateTime(2013, 08, 05, 05, 15, 10);
+            var sub = dt1.Subtract(dt2);
+            Console.WriteLine(sub.ToString("c"));
+            Console.WriteLine(sub.ToString(@"d\.h\:m\:s"));
+        }
+    }
+}
