76abbce50cdee13d6a11231a54d2fd2996be98e0
blue32a <blue32a.jp@gmail.com>
2026-05-22 23:55:21 +0900

    リスト5.41 任意の書式文字列から日付／時刻オブジェクトを取得する

---
+++ Chap05/TimeExact.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace SelfCSharp.Chap05
+{
+    internal class TimeExact
+    {
+        static void Main(string[] args)
+        {
+            var str = "20260522235117";
+            DateTime dt = DateTime.ParseExact(str, "yyyyMMddHHmmss", new CultureInfo("Ja-JP"));
+            Console.WriteLine(dt);
+        }
+    }
+}
