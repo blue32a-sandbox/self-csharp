864076501f83482585030f0d7ea9db2d43b990a2
blue32a <blue32a.jp@gmail.com>
2026-05-23 11:27:06 +0900

    リスト5.57 静的メンバーを提供するDirectoryクラス

---
+++ Chap05/DirectoryStaticProcess.cs
@@ -0,0 +1,27 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class DirectoryStaticProcess
+    {
+        static void Main(string[] args)
+        {
+            var dir = @"C:\data\SelfCSharp";
+            Console.WriteLine(Directory.Exists(dir));
+            Console.WriteLine(Directory.GetParent(dir));
+            Console.WriteLine(Directory.GetDirectoryRoot(dir));
+            Console.WriteLine(Directory.GetCreationTime(dir));
+            Console.WriteLine(Directory.GetLastAccessTime(dir));
+            Console.WriteLine(Directory.GetLastWriteTime(dir));
+
+            var dirs = Directory.GetDirectories(dir);
+            foreach (var d in dirs)
+            {
+                Console.WriteLine(d);
+            }
+
+            Directory.CreateDirectory(@"C:\data\smp");
+            Directory.Move(@"C:\data\smp", @"C:\data\test");
+            Directory.Move(@"C:\data\test", @"C:\data\SelfCSharp\test");
+            Directory.Delete(@"C:\data\SelfCSharp\test");
+        }
+    }
+}
