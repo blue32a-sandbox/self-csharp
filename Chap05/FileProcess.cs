8279a77a3ced74edd488b5b8a53728352a3ef65c
blue32a <blue32a.jp@gmail.com>
2026-05-23 10:51:15 +0900

    リスト5.54 ファイルの操作

---
+++ Chap05/FileProcess.cs
@@ -0,0 +1,30 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class FileProcess
+    {
+        static void Main(string[] args)
+        {
+            var file = new FileInfo(@"C:\data\sample.txt");
+
+            Console.WriteLine(file.Exists);
+            Console.WriteLine(file.Name);
+            Console.WriteLine(file.DirectoryName);
+            Console.WriteLine(file.IsReadOnly);
+            Console.WriteLine(file.LastAccessTime);
+            Console.WriteLine(file.LastWriteTime);
+            Console.WriteLine(file.Length);
+
+            var file2 = file.CopyTo(@"C:\data\sample_copy.txt", true);
+            Console.WriteLine(file2.FullName);
+
+            file2.MoveTo(@"C:\data\SelfCSharp\sample_copy.txt");
+            Console.WriteLine(file2.FullName);
+
+            file2.MoveTo(@"C:\data\SelfCSharp\sample_renamed.txt");
+            Console.WriteLine(file2.FullName);
+
+            file2.Delete();
+            Console.WriteLine(file2.Exists);
+        }
+    }
+}
