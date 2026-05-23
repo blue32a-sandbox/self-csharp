5e512af3cca8bfc5951b833bdc1607ea44fe35c6
blue32a <blue32a.jp@gmail.com>
2026-05-23 17:42:57 +0900

    リスト6.14 和集合／差集合／積集合などを求める

---
+++ Chap06/SetUnion.cs
@@ -0,0 +1,27 @@
+﻿namespace SelfCSharp.Chap06
+{
+    internal class SetUnion
+    {
+        static void Main(string[] args)
+        {
+            var hs = new HashSet<int> { 1, 10, 15, 20, 30 };
+
+            hs.IntersectWith(new HashSet<int>() { 1, 10, 20, 30, 60 });
+            PrintSet(hs); // 1,10,20,30
+
+            hs.ExceptWith(new HashSet<int> { 15, 30 });
+            PrintSet(hs); // 1,10,20
+
+            hs.UnionWith(new HashSet<int> { 10, 15, 30 });
+            PrintSet(hs); // 1,10,30,20,15
+
+            hs.SymmetricExceptWith(new HashSet<int> { 1, 10, 30 });
+            PrintSet(hs); // 20,15
+        }
+
+        private static void PrintSet(HashSet<int> hs)
+        {
+            Console.WriteLine(String.Join(",", hs));
+        }
+    }
+}
