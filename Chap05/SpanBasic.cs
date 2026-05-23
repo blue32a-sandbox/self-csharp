57bda0c19a9bc4a5dd4d0cf3cfc5fca13e8c8e0d
blue32a <blue32a.jp@gmail.com>
2026-05-23 15:22:29 +0900

    リスト5.62 特定範囲のデータを編集するSpan<T>構造体

---
+++ Chap05/SpanBasic.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class SpanBasic
+    {
+        static void Main(string[] args)
+        {
+            var list = new[] { 10, 20, 30, 40, 50, 60 };
+
+            // int配列から２～４番目の要素を参照するSpanを生成
+            var sp = new Span<int> (list, 2, 3);
+            foreach (var v in sp)
+            {
+                Console.WriteLine(v); // 30 40 50
+            }
+
+            // Span経由でint配列を更新
+            sp[1] = 999;
+            foreach (var v in list)
+            {
+                Console.WriteLine(v); // 10 20 30 999 50 60
+            }
+        }
+    }
+}
