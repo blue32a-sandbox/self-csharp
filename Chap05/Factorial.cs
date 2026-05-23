4fd2b709bd3dbfdee51bfbbc37f54f77916e667b
blue32a <blue32a.jp@gmail.com>
2026-05-23 15:00:05 +0900

    リスト5.60 long型委譲の整数を演算するBigInteger構造体

---
+++ Chap05/Factorial.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace SelfCSharp.Chap05
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            BigInteger result = 1;
+            for (var i = 1; i < 26; i++)
+            {
+                result *= i;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
