fbe483062d3c4b83f1e9c3c542af3e266cad6ac0
blue32a <blue32a.jp@gmail.com>
2026-05-23 10:19:30 +0900

    リスト5.50 テキストをファイルに書き込む

---
+++ Chap05/StreamWrite.cs
@@ -0,0 +1,13 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class StreamWrite
+    {
+        static void Main(string[] args)
+        {
+            using (var writer = new StreamWriter(@"c:\data\data.log", true))
+            {
+                writer.WriteLine(DateTime.Now.ToString());
+            }
+        }
+    }
+}
