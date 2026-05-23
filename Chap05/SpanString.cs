196ee90ade946ac340dc4f4a1275b227f0c0125f
blue32a <blue32a.jp@gmail.com>
2026-05-23 15:28:05 +0900

    リスト5.63 文字列をSpanに変換し操作する

---
+++ Chap05/SpanString.cs
@@ -0,0 +1,13 @@
+﻿namespace SelfCSharp.Chap05
+{
+    internal class SpanString
+    {
+        static void Main(string[] args)
+        {
+            var str = "WINGSプロジェクト";
+            var sp = str.AsSpan(5, 5);
+            // 文字列は不変なので、戻り値は読み取り専用のReadOnlySpan
+            sp[2] = 'ゴ'; // エラー
+        }
+    }
+}
