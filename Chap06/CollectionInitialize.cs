107cb4f142a57ce67d856a3740902341519748d4
blue32a <blue32a.jp@gmail.com>
2026-05-23 16:28:52 +0900

    リスト6.3 コレクションの初期化

---
+++ Chap06/CollectionInitialize.cs
@@ -0,0 +1,16 @@
+﻿namespace SelfCSharp.Chap06
+{
+    internal class CollectionInitialize
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>()
+            {
+                "あいうえお",
+                "かきくけこ",
+                "さしすせそ",
+            };
+            Console.WriteLine(list[0]); // あいうえお
+        }
+    }
+}
