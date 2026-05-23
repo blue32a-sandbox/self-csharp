6823568c35ba27f73889caeef40d552541026a52
blue32a <blue32a.jp@gmail.com>
2026-05-23 16:32:17 +0900

    リスト6.4,6.5 インデックス初期化子

---
+++ Chap06/CollectionInitializeIndex.cs
@@ -0,0 +1,16 @@
+﻿namespace SelfCSharp.Chap06
+{
+    internal class CollectionInitializeIndex
+    {
+        static void Main(string[] args)
+        {
+            var flower = new Dictionary<string, string>()
+            {
+                ["Rose"] = "バラ",
+                ["Sunflower"] = "ひまわり",
+                ["Morning Glory"] = "あさがお",
+            };
+            Console.WriteLine(flower["Rose"]); // バラ
+        }
+    }
+}
