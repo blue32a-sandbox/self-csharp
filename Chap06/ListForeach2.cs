578a9e867cdce534fe4502a54a6c4e80815b1fc7
blue32a <blue32a.jp@gmail.com>
2026-05-23 16:39:17 +0900

    リスト6.7 列挙子を利用してコレクションを順に処理する

---
+++ Chap06/ListForeach2.cs
@@ -0,0 +1,21 @@
+﻿namespace SelfCSharp.Chap06
+{
+    internal class ListForeach2
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>
+            {
+                "バラ",
+                "ひまわり",
+                "あさがお",
+            };
+            var enu = list.GetEnumerator();
+            while (enu.MoveNext())
+            {
+                var str = enu.Current;
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
