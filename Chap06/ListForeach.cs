f3bc9590555e7aaae737a7b1f86cfeb7c5894668
blue32a <blue32a.jp@gmail.com>
2026-05-23 16:34:52 +0900

    リスト6.6 コレクションを順に処理する

---
+++ Chap06/ListForeach.cs
@@ -0,0 +1,20 @@
+﻿namespace SelfCSharp.Chap06
+{
+    internal class ListForeach
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>
+            {
+                "バラ",
+                "ひまわり",
+                "あさがお",
+            };
+
+            foreach (var s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
