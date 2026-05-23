dcc8650a1d76f20ab50b9786b62a001a03cb3c25
blue32a <blue32a.jp@gmail.com>
2026-05-23 15:50:11 +0900

    リスト6.2 ジェネリック版コレクション

---
+++ Chap06/GenericBasic.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap06
+{
+    internal class GenericBasic
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>();
+            list.Add("あいうえお");
+            string str = list[0]; // キャスト不要
+            Console.WriteLine(str);
+            //list.Add(12345); // コンパイルエラー
+        }
+    }
+}
