6af817a8db4f1a9c41317a7441cb3a78b7378687
blue32a <blue32a.jp@gmail.com>
2026-05-23 20:28:37 +0900

    リスト7.1 最小限のクラス

---
+++ Chap07/ClassBasic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SelfCSharp.Chap07
+{
+    internal class Person
+    {
+    }
+
+    internal class ClassBasic
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+        }
+    }
+}
