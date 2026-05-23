e2305a7e3762d8b815468c60ad6c941b2453c094
blue32a <blue32a.jp@gmail.com>
2026-05-23 20:15:38 +0900

    リスト6.19 リストの内容を文字列長でソートする

---
+++ Chap06/ListSort.cs
@@ -0,0 +1,30 @@
+﻿namespace SelfCSharp.Chap06
+{
+    internal class StringLengthComparer2 : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            return x.Length - y.Length;
+        }
+    }
+
+    internal class ListSort
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>()
+            {
+                "バラ",
+                "ひまわり",
+                "あざみ",
+            };
+
+            list.Sort(new StringLengthComparer2());
+
+            foreach (var s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
