0fa65192f0534fed24dadc0d79667ca0681ca2ee
blue32a <blue32a.jp@gmail.com>
2026-05-23 15:46:30 +0900

    リスト6.1 ジェネリック登場以前のコレクション

---
+++ Chap06/GenericNoUse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace SelfCSharp.Chap06
+{
+    internal class GenericNoUse
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("あいうえお");
+            list.Add("かきくけこ");
+            string str = (string)list[0];
+            Console.WriteLine(str);
+
+            list.Add(12345);
+            string str2 = (string)list[2]; // 実行時エラー
+        }
+    }
+}
