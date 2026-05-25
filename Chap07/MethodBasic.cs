b4728309995023b453b80f089fa74a04eee0bf0a
blue32a <blue32a.jp@gmail.com>
2026-05-25 20:51:46 +0900

    リスト7.4,7.5 メソッドの定義とアクセス

---
+++ Chap07/MethodBasic.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap07.ClassMethod
+{
+    internal class Person
+    {
+        public string firstName = "";
+        public string lastName = "";
+
+        public string Show()
+        {
+            return $"名前は{this.lastName}{this.firstName}です。";
+        }
+    }
+
+    internal class MethodBasic
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.firstName = "太郎";
+            p.lastName = "山田";
+            Console.WriteLine(p.Show());
+        }
+    }
+}
