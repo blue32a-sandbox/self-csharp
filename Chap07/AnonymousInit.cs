2c17e44ed43f5b8c9089e65a02f830127e651d3a
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:53:05 +0900

    リスト7.20 初期化の順番

---
+++ Chap07/AnonymousInit.cs
@@ -0,0 +1,25 @@
+﻿namespace SelfCSharp.Chap07.Anonymous
+{
+    internal class Hoge
+    {
+        public string value = "fiald";
+        
+        public Hoge() { }
+
+        public Hoge(string value)
+        {
+            this.value = value;
+        }
+    }
+
+    internal class AnonymousInit
+    {
+        static void Main(string[] args)
+        {
+            var h = new Hoge("constructor") { value = "initalizer" };
+
+            // 優先順位はオブジェクト初期化子、コンストラクター、フィールドの順
+            Console.WriteLine(h.value); // initalizer
+        }
+    }
+}
