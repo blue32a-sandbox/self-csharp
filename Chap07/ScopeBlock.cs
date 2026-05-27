90378e2098a2aa3487db3b7c6eebf16df4a1df6d
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:17:15 +0900

    リスト7.14 ブロックスコープ

---
+++ Chap07/ScopeBlock.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class ScopeBlock
+    {
+        static void Main(string[] args)
+        {
+            var data = "ローカルスコープ";
+            {
+                // ブロックスコープの変数はローカル変数の一種であり、同じ名前を宣言することはできない
+                // var data = "ブロックスコープ";  // コンパイルエラー
+            }
+        }
+    }
+}
