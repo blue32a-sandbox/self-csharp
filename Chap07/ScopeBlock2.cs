d0ca3ec67e82f261f9b796040544d36ece74caf8
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:19:23 +0900

    リスト7.15 ブロックスコープ（宣言の順番を変更）

---
+++ Chap07/ScopeBlock2.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class ScopeBlock2
+    {
+        static void Main(string[] args)
+        {
+            {
+                var data = "ブロックスコープ";
+            }
+            // 宣言の順番を替えても同じ名前を宣言することはできない
+            // var data = "ローカルスコープ"; // コンパイルエラー
+        }
+    }
+}
