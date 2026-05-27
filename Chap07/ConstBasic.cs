21ea85ef915cea472c6cf293881606260e743e73
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:27:30 +0900

    リスト7.17 コンストラクターでフィールドを初期化

---
+++ Chap07/ConstBasic.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfCSharp.Chap07.ClassConst
+{
+    internal class Person
+    {
+        public string fristName;
+        public string lastName;
+
+        public Person(string fristName, string lastName)
+        {
+            this.fristName = fristName;
+            this.lastName = lastName;
+        }
+
+        public string Show()
+        {
+            return $"名前は{this.lastName}{this.fristName}です。";
+        }
+    }
+
+    internal class ConstBasic
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("太郎", "山田");
+            Console.WriteLine(p.Show()); // 名前は山田太郎です。
+        }
+    }
+}
