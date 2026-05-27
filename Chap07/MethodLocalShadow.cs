a2e7be42a29d036ed8150937096ec81c4686ecba
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:10:16 +0900

    リスト7.13 変数の隠蔽

---
+++ Chap07/MethodLocalShadow.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class MethodLocalShadow
+    {
+        static void Main(string[] args)
+        {
+            var msg = "Local Variable";
+
+            void MyLocal()
+            {
+                var msg = "Inner Variable";
+                // C# 8.0 以降では隠蔽とみなされ、名前の重複が許容される
+                Console.WriteLine(msg); // Inner Variable
+            }
+            MyLocal();
+            Console.WriteLine(msg); // Local Variable
+        }
+    }
+}
