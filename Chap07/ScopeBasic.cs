1194e29e242dae85d363a9ca336d77655cb2d21f
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:06:59 +0900

    リスト7.11,7.12 フィールドとローカル変数

---
+++ Chap07/ScopeBasic.cs
@@ -0,0 +1,27 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class ScopeBasic
+    {
+        public string data = "フィールド";
+
+        public string Show()
+        {
+            string data = "ローカル";
+            return data;
+        }
+
+        public string Show2()
+        {
+            string data = "ローカル";
+            return this.data;
+        }
+
+        static void Main(string[] args)
+        {
+            var s = new ScopeBasic();
+            Console.WriteLine(s.Show()); // ローカル
+            Console.WriteLine(s.Show2()); // フィールド
+            Console.WriteLine(s.data); // フィールド
+        }
+    }
+}
