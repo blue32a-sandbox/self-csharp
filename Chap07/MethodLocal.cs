e78b3f3494226aa8cd493b56f913b7347c3d7977
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:03:20 +0900

    リスト7.9,7.10 ローカル関数

---
+++ Chap07/MethodLocal.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class MethodLocal
+    {
+        static void Main(string[] args)
+        {
+            // 宣言位置より前で呼び出し
+            Console.WriteLine(Discount(1000, 0.2)); // 800
+
+            double Discount(int price, double rate)
+            {
+                return price * (1 - rate);
+            }
+
+            Console.WriteLine(Discount(1000, 0.2)); // 800
+        }
+    }
+}
