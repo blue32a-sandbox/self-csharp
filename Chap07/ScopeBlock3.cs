f7c0426aa56da304a9bf4dad55cb8a93436b04d5
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:23:07 +0900

    リスト7.16 ブロックスコープ（並列関係にあるブロック）

---
+++ Chap07/ScopeBlock3.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class ScopeBlock3
+    {
+        static void Main(string[] args)
+        {
+            {
+                var data = "ローカルスコープ";
+                Console.WriteLine(data); // ローカルスコープ
+            }
+            {
+                var data = "ローカルスコープ2";
+                Console.WriteLine(data); // ローカルスコープ2
+            }
+        }
+    }
+}
