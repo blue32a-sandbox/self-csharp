81ad0ab4792e365cc84857af54219a52d0abae70
blue32a <blue32a.jp@gmail.com>
2026-05-27 23:41:10 +0900

    リスト7.18 コンストラクター初期化子

---
+++ Chap07/ConstructorBasic.cs
@@ -0,0 +1,31 @@
+﻿namespace SelfCSharp.Chap07.Construct
+{
+    internal class Person
+    {
+        public string fristName;
+        public string lastName;
+
+        public Person(string fristName, string lastName)
+        {
+            this.fristName = fristName;
+            this.lastName = lastName;
+        }
+
+        // コンストラクター初期化子を使ってオーバーロードされたコンストラクター
+        public Person() : this("権兵衛", "名無") { }
+
+        public void Show()
+        {
+            Console.WriteLine($"名前は{this.lastName}{this.fristName}です。");
+        }
+    }
+
+    internal class ConstructorBasic
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Show(); // 名前は名無権兵衛です。
+        }
+    }
+}
