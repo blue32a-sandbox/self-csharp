aef2649060db5260e296e220eb5d1028406b39cc
blue32a <blue32a.jp@gmail.com>
2026-05-30 22:38:30 +0900

    リスト7.39 引数の省略をより早く検知する

---
+++ Chap07/ArgsParamsGood.cs
@@ -0,0 +1,22 @@
+﻿namespace SelfCSharp.Chap07.MethodArgs
+{
+    internal class ArgsParamsGood
+    {
+        public int TotalProducts(int initial, params int[] values)
+        {
+            int result = initial;
+            foreach (var value in values)
+            {
+                result *= value;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var v = new ArgsParamsGood();
+            Console.WriteLine(v.TotalProducts(1, 5, 7, 8, 2)); // 560
+            //v.TotalProducts(); // コンパイルエラー
+        }
+    }
+}
