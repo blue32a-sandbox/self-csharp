c1d9ec80c78e47144bd475cd14ff4e82e0ac4797
blue32a <blue32a.jp@gmail.com>
2026-05-30 19:28:46 +0900

    リスト7.23 クラスフィールド

--- Chap07/StaticBasic.cs
+++ Chap07/StaticBasic.cs
@@ -2,6 +2,12 @@
 {
     internal class Figure
     {
+        public static double Pi = 3.14;
+        public static void GetCircleArea(double r)
+        {
+            Console.WriteLine($"円の面積は{ r * r * Pi }");
+        }
+
         public static void GetTriangleArea(int width, int height)
         {
             Console.WriteLine($"三角形の面積は{width * height / 2}");
@@ -12,6 +18,8 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Figure.Pi); // 3.14
+            Figure.GetCircleArea(5); // 円の面積は78.5 
             Figure.GetTriangleArea(10, 20); // 三角形の面積は100
         }
     }
