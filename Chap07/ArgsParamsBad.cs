8a8ab15ceb91c45d74845568352e487f5e434efc
blue32a <blue32a.jp@gmail.com>
2026-05-30 22:32:54 +0900

    リスト7.38 可変長引数の省略

---
+++ Chap07/ArgsParamsBad.cs
@@ -0,0 +1,25 @@
+﻿namespace SelfCSharp.Chap07.MethodArgs
+{
+    internal class ArgsParamsBad
+    {
+        public int TotalProducts(params int[] values)
+        {
+            if (values.Length == 0)
+            {
+                throw new ArgumentNullException();
+            }
+            int result = 1;
+            foreach (var value in values)
+            {
+                result *= value;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var v = new ArgsParamsBad();
+            v.TotalProducts(); // 例外が発生する
+        }
+    }
+}
