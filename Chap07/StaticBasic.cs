a6ea2487eb9fa5ffb05f789d04d59ce27cc21755
blue32a <blue32a.jp@gmail.com>
2026-05-30 19:26:39 +0900

    リスト7.22 クラスメソッド

---
+++ Chap07/StaticBasic.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap07.MethodStatic
+{
+    internal class Figure
+    {
+        public static void GetTriangleArea(int width, int height)
+        {
+            Console.WriteLine($"三角形の面積は{width * height / 2}");
+        }
+    }
+
+    internal class StaticBasic
+    {
+        static void Main(string[] args)
+        {
+            Figure.GetTriangleArea(10, 20); // 三角形の面積は100
+        }
+    }
+}
