8754a970c277ff9fcdbe2fbc4f0c97996c8ebefa
blue32a <blue32a.jp@gmail.com>
2026-05-30 19:57:44 +0900

    リスト7.27 const定数にstatic修飾子は指定できない

---
+++ Chap07/ConstantsBasic.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class Constants
+    {
+        // const定数をは無条件にstaticであるため、static修飾子は不要
+        //public static const int Value = 10; // エラー
+        public const int Value = 10;
+    }
+
+    internal class ConstantsBasic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Constants.Value);
+        }
+    }
+}
