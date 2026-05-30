ed35a03c50ef20afccdf517aed6e5878124ee742
blue32a <blue32a.jp@gmail.com>
2026-05-30 22:45:09 +0900

    リスト7.40 値型の値渡し

---
+++ Chap07/PassBasic.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class PassBasic
+    {
+        public int CountUp(int data)
+        {
+            data++;
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            var data = 1;
+            var p = new PassBasic();
+            Console.WriteLine(p.CountUp(data)); // 2
+            Console.WriteLine(data); // 1
+        }
+    }
+}
