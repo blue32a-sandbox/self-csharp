3abfb0259ff0bc8639067fdee507b6693766905a
blue32a <blue32a.jp@gmail.com>
2026-05-30 22:20:44 +0900

    リスト7.36 名前付き引数

---
+++ Chap07/ArgsNamed.cs
@@ -0,0 +1,16 @@
+﻿namespace SelfCSharp.Chap07.MethodArgs
+{
+    internal class ArgsNamed
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person()
+            {
+                lastName = "山田",
+                fristName = "太郎"
+            };
+            p.Show(title: "氏"); // こんにちは、山田太郎氏！
+            p.Show(title: "氏", greeting: "おはよう"); // おはよう、山田太郎氏！
+        }
+    }
+}
