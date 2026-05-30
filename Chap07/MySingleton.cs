553557e27afe1c19f743d82f251867f83b550e13
blue32a <blue32a.jp@gmail.com>
2026-05-30 19:33:07 +0900

    リスト7.24 クラスフィールドを使ったシングルトンパターン

---
+++ Chap07/MySingleton.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class MySingleton
+    {
+        private static MySingleton instance = new MySingleton();
+
+        private MySingleton() { }
+
+        public static MySingleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var s1 = MySingleton.Instance;
+            var s2 = MySingleton.Instance;
+            Console.WriteLine(s1 == s2); // True
+        }
+    }
+}
