b28e1091d5d1b3b11b0c1ebfd9d102a7d031bdd7
blue32a <blue32a.jp@gmail.com>
2026-05-30 22:17:20 +0900

    リスト7.35 引数の既定値

---
+++ Chap07/ArgsDefault.cs
@@ -0,0 +1,27 @@
+﻿namespace SelfCSharp.Chap07.MethodArgs
+{
+    internal class Person
+    {
+        public string fristName = "";
+        public string lastName = "";
+
+        public void Show(string greeting = "こんにちは", string title = "さん")
+        {
+            Console.WriteLine($"{greeting}、{this.lastName}{this.fristName}{title}！");
+        }
+    }
+
+    internal class ArgsDefault
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person()
+            {
+                lastName = "山田",
+                fristName = "太郎"
+            };
+            p.Show(); // こんにちは、山田太郎さん！
+            p.Show("はじめまして"); // はじめまして、山田太郎さん！
+        }
+    }
+}
