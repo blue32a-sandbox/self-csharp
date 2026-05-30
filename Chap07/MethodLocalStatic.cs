7e0ec1b92a04a9302b0e343945dcfa5132007349
blue32a <blue32a.jp@gmail.com>
2026-05-30 22:12:28 +0900

    リスト7.34 staticメソッドは関数外部の変数に依存しない

---
+++ Chap07/MethodLocalStatic.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class MethodLocalStatic
+    {
+        int myfield = 0;
+        static int myfield2 = 0;
+
+        static void Main(string[] args)
+        {
+            var mylocal = 1;
+            const int MY_CONST = 2;
+
+            static void Hoge()
+            {
+                //Console.WriteLine(myfield); // エラー
+                //Console.WriteLine(mylocal); // エラー
+                Console.WriteLine(myfield2); // 0
+                Console.WriteLine(MY_CONST); // 2
+                Console.WriteLine(nameof(mylocal)); // mylocal
+            }
+            Hoge();
+        }
+    }
+}
