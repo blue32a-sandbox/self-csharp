11bb3f69673645d1c26cb04da835dd82b85f2d0d
blue32a <blue32a.jp@gmail.com>
2026-05-30 23:25:33 +0900

    リスト7.41 参照型の値渡し

---
+++ Chap07/PassArray.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class PassArray
+    {
+        public int[] Update(int[] data)
+        {
+            data[0] = 5;
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] { 2, 4, 6 };
+            var p = new PassArray();
+            Console.WriteLine(p.Update(data)[0]); // 5
+            Console.WriteLine(data[0]); // 5
+        }
+    }
+}
