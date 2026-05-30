ab811395b25dfaae6ed9b1d648f95d767da63865
blue32a <blue32a.jp@gmail.com>
2026-05-30 19:39:32 +0900

    リスト7.25,7.26 クラス定数

---
+++ Chap07/FieldStatic.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07.ClassField
+{
+    internal class MyApp
+    {
+        public static readonly string Title = "独習C#";
+
+        // readonly修飾子を、const修飾子と置き換えてもほぼ同じ
+        //public const string Title = "独習C#";
+    }
+
+    internal class FieldStatic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyApp.Title); // 独習C#
+            //MyApp.Title = "本気でおぼえるC#"; // コンパイルエラー
+        }
+    }
+}
