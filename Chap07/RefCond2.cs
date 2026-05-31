5aa3bef612e9bf4129c589270a76131a95f81dcc
blue32a <blue32a.jp@gmail.com>
2026-05-31 23:36:03 +0900

    リスト7.52 条件演算子での参照渡し２

---
+++ Chap07/RefCond2.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class RefCond2
+    {
+        static void Main(string[] args)
+        {
+            var i = 10;
+            var j = 20;
+
+            (i > j ? ref i : ref j) = 99;
+            Console.WriteLine(j); // 99
+        }
+    }
+}
