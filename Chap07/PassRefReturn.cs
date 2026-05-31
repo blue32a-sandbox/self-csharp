ee1e30b3c6a5f96c58c27b2543fcab15ee2d8fa5
blue32a <blue32a.jp@gmail.com>
2026-05-31 23:23:35 +0900

    リスト7.47 戻り値の参照渡し

---
+++ Chap07/PassRefReturn.cs
@@ -0,0 +1,20 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class PassRefReturn
+    {
+        public ref int ReturnRef(int[] data)
+        {
+            return ref data[0];
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] { 1, 2, 3 };
+            var p = new PassRefReturn();
+            ref int num = ref p.ReturnRef(data);
+            num = 10;
+            Console.WriteLine(num); // 10
+            Console.WriteLine(data[0]); // 10
+        }
+    }
+}
