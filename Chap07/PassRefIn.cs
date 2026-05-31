32b1055597aee1498128e25553eece3aefa48581
blue32a <blue32a.jp@gmail.com>
2026-05-31 23:18:34 +0900

    リスト7.46 読み取り専用の参照渡し in修飾子

---
+++ Chap07/PassRefIn.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfCSharp.Chap07
+{
+    struct Coordinate
+    {
+        public double X;
+        public double Y;
+
+        public Coordinate(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+
+    internal class PassRefIn
+    {
+        public static void Show(in Coordinate c)
+        {
+            Console.WriteLine($"[{c.X}, {c.Y}]");
+            //c.X = 10; // 読み取り専用なのでエラー
+        }
+
+        static void Main(string[] args)
+        {
+            Show(new Coordinate(10, 15)); // [10, 15]
+        }
+    }
+}
