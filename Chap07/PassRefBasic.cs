5230ddb0f9f6651574f82b42514200bcbf97ca38
blue32a <blue32a.jp@gmail.com>
2026-05-31 23:07:13 +0900

    リスト7.43 値型の参照渡し

---
+++ Chap07/PassRefBasic.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class PassRefBasic
+    {
+        public int CountUp(ref int data)
+        {
+            data++;
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            var data = 1;
+            var p = new PassRefBasic();
+            Console.WriteLine(p.CountUp(ref data)); // 2
+            Console.WriteLine(data); // 2
+        }
+    }
+}
