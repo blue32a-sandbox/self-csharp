27ff0e9e0365fa206e3f0aa93588b5f3aad2ddcd
blue32a <blue32a.jp@gmail.com>
2026-05-31 23:33:47 +0900

    リスト7.51 条件演算子での参照渡し

---
+++ Chap07/RefCond.cs
@@ -0,0 +1,15 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class RefCond
+    {
+        static void Main(string[] args)
+        {
+            var i = 10;
+            var j = 20;
+
+            ref var k = ref i > j ? ref i : ref j;
+            k = 99;
+            Console.WriteLine($"i={i}, j={j}, k={k}"); // i=10, j=99, k=99
+        }
+    }
+}
