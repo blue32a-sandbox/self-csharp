eb1fbe131199d397eca3324a6e07ecb087190a6e
blue32a <blue32a.jp@gmail.com>
2026-05-31 23:11:41 +0900

    リスト7.44 参照型の参照渡し

---
+++ Chap07/PassRefArray.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class PassRefArray
+    {
+        public int[] Update(ref int[] data)
+        {
+            data[0] = 5;
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] { 2, 4, 6 };
+            var p = new PassRefArray();
+            Console.WriteLine(p.Update(ref data)[0]); // 5
+            Console.WriteLine(data[0]); // 5
+        }
+    }
+}
