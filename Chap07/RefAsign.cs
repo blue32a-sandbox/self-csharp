50b22f1502a89d7a71b4aa3e2c78a79dd276f114
blue32a <blue32a.jp@gmail.com>
2026-05-31 23:31:45 +0900

    リスト7.50 参照の再代入

---
+++ Chap07/RefAsign.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class RefAsign
+    {
+        static void Main(string[] args)
+        {
+            var i = 10;
+            var j = 20;
+
+            ref var k = ref i;
+            k = 15;
+            Console.WriteLine($"i={i}, j={j}, k={k}"); // i=15, j=20, k=15
+
+            k = ref j;
+            k = 108;
+            Console.WriteLine($"i={i}, j={j}, k={k}"); // i=15, j=108, k=108
+        }
+    }
+}
