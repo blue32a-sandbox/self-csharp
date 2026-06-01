a56097553d46d89f3fdf6d6305edc475b9320a44
blue32a <blue32a.jp@gmail.com>
2026-06-01 22:18:56 +0900

    リスト7.56 タプルによる変数の入れ替え

---
+++ Chap07/TupleSwap.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class TupleSwap
+    {
+        static void Main(string[] args)
+        {
+            var x = 1;
+            var y = 20;
+            (y, x) = (x, y);
+            Console.WriteLine(x); // 20
+            Console.WriteLine(y); // 1
+        }
+    }
+}
