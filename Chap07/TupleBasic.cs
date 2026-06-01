0cbc3392735c09e8b63480d546658641265fcd91
blue32a <blue32a.jp@gmail.com>
2026-06-01 22:11:12 +0900

    リスト7.55 タプル型

---
+++ Chap07/TupleBasic.cs
@@ -0,0 +1,22 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class TupleBasic
+    {
+        public (int max, int min) GetMaxMin(int x, int y)
+        {
+            return x >= y ? (x, y) : (y, x);
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new TupleBasic();
+            var t = a.GetMaxMin(15, 13);
+            Console.WriteLine(t.max); // 15
+            Console.WriteLine(t.min); // 13
+
+            var (resultMax, resultMin) = a.GetMaxMin(5, 3);
+            Console.WriteLine(resultMax); // 5
+            Console.WriteLine(resultMin); // 3
+        }
+    }
+}
