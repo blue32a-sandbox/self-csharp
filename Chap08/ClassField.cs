f673dec15e7dc2f0808416a3e895d43ff1d2e986
blue32a <blue32a.jp@gmail.com>
2026-06-01 22:59:55 +0900

    リスト8.1 インスタンスフィールドは原則公開すべきではない

---
+++ Chap08/ClassField.cs
@@ -0,0 +1,30 @@
+﻿namespace SelfCSharp.Chap08.PublicField
+{
+    internal class  Triangle
+    {
+        // インスタンスフィールドを公開すると
+        // * 読み書きを制御できない
+        // * 値の妥当性をチェックできない
+        // * 内部表現の変更に弱い
+        public double width;
+        public double height;
+
+        public double GetArea()
+        {
+            return this.width * this.height / 2;
+        }
+    }
+
+    internal class ClassField
+    {
+        static void Main(string[] args)
+        {
+            var t = new Triangle()
+            {
+                width = 20,
+                height = 10
+            };
+            Console.WriteLine(t.GetArea());
+        }
+    }
+}
