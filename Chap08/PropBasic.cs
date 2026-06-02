e590e722b3ec82250329119ae4b07b5c53d6a7cb
blue32a <blue32a.jp@gmail.com>
2026-06-02 23:03:04 +0900

    リスト8.3 プロパティを使った値の妥当性チェック

---
+++ Chap08/PropBasic.cs
@@ -0,0 +1,54 @@
+﻿namespace SelfCSharp.Chap08.Prop
+{
+    internal class  Triangle
+    {
+        private double _width;
+        private double _height;
+
+        public double Width
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("正数で指定してください。");
+                }
+                this._width = value;
+            }
+
+            get {  return _width; }
+        }
+
+        public double Height
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("正数で指定してください。");
+                }
+                this._height = value;
+            }
+
+            get { return _height; }
+        }
+
+        public double GetArea()
+        {
+            return Width * Height / 2;
+        }
+    }
+
+    internal class PropBasic
+    {
+        static void Main(string[] args)
+        {
+            var t = new Triangle();
+            t.Width = 10;
+            t.Height = 5;
+            Console.WriteLine($"三角形の面積は{t.GetArea()}です。");
+
+            t.Width = -5; // エラー
+        }
+    }
+}
