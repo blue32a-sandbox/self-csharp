c17bfd5f58c29f87c43ad45663e9ca550100fc15
blue32a <blue32a.jp@gmail.com>
2026-06-20 23:22:11 +0900

    リスト8.8 文字列型のインデックスを受け取るインデクサー

---
+++ Chap08/IndexerString.cs
@@ -0,0 +1,25 @@
+﻿namespace SelfCSharp.Chap08
+{
+    internal class JapaneseMonth
+    {
+        private string[] _month = { "睦月", "如月", "弥生", "卯月", "皐月",
+            "水無月", "文月", "葉月", "長月", "神無月", "霜月", "師走" };
+
+        public int this[string name]
+        { 
+            get
+            {
+                return Array.IndexOf(this._month, name) + 1;
+            }
+        }
+    }
+
+    internal class IndexerString
+    {
+        static void Main(string[] args)
+        {
+            var mon = new JapaneseMonth();
+            Console.WriteLine(mon["如月"]); // 2
+        }
+    }
+}
