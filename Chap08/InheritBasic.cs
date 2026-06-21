50055b23e5ddf19c26167192d1d937e393e6136c
blue32a <blue32a.jp@gmail.com>
2026-06-21 10:16:39 +0900

    リスト8.11 継承の基本

---
+++ Chap08/InheritBasic.cs
@@ -0,0 +1,26 @@
+﻿using SelfCSharp.Chap08.Prop;
+
+namespace SelfCSharp.Chap08
+{
+    internal class BusinessPerson : Person
+    {
+        public string Work()
+        {
+            return $"{this.LastName}{this.FirstName}は、働きます。";
+        }
+    }
+
+    internal class InheritBasic
+    {
+        static void Main(string[] args)
+        {
+            var bp = new BusinessPerson
+            {
+                FirstName = "太郎",
+                LastName = "山田"
+            };
+            Console.WriteLine(bp.Work()); // 山田太郎は、働きます。
+            Console.WriteLine(bp.Show()); // 名前は山田太郎です。
+        }
+    }
+}
