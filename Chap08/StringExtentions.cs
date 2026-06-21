585603aa6c3fb308758ec051a57e3410ab7a0dbb
blue32a <blue32a.jp@gmail.com>
2026-06-21 17:44:56 +0900

    リスト8.32 拡張メソッドの実装

---
+++ Chap08/StringExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace SelfCSharp.Chap08.ExtensionBasic
+{
+    internal static class StringExtentions
+    {
+        public static string Repeat(this string str, int count)
+        {
+            var builder = new StringBuilder();
+            for (var i = 1; i <= count; i++)
+            {
+                builder.Append(str);
+            }
+            return builder.ToString();
+        }
+    }
+}
