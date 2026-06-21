501dc1007419f5a87c6d8097e54ec55dc6a8f0c1
blue32a <blue32a.jp@gmail.com>
2026-06-21 11:29:50 +0900

    リスト8.21 派生クラスから基底クラスへのアップキャスト

---
+++ Chap08/CastUp.cs
@@ -0,0 +1,31 @@
+﻿namespace SelfCSharp.Chap08.Cast
+{
+    internal class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+        public virtual string Show()
+        {
+            return $"名前は{this.LastName}{this.FirstName}です。";
+
+        }
+    }
+
+    internal class BusinessPerson : Person
+    {
+        public override string Show()
+        {
+            return $"会社員の{this.LastName}{this.FirstName}です。";
+        }
+    }
+
+    internal class CastUp
+    {
+        static void Main(string[] args)
+        {
+            Person bp = new BusinessPerson() { FirstName = "太郎", LastName = "山田" };
+            Console.WriteLine(bp.Show()); // 会社員の山田太郎です。
+        }
+    }
+}
