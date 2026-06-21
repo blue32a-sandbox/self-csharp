166b0c069e8bee046f76feb4288204c9e870939b
blue32a <blue32a.jp@gmail.com>
2026-06-21 18:24:04 +0900

    リスト8.35 抽象メソッド

---
+++ Chap08/AbstractBasic.cs
@@ -0,0 +1,50 @@
+﻿namespace SelfCSharp.Chap08.Abstract
+{
+    internal abstract class Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public abstract double GetArea();
+    }
+
+    internal class Triangle : Figure
+    {
+        public Triangle(double width, double height)
+            : base(width, height) { }
+
+        public override double GetArea()
+        {
+            return this.Width * this.Height / 2;
+        }
+    }
+
+    internal class Square : Figure
+    {
+        public Square(double width, double height)
+            : base(width, height) { }
+
+        public override double GetArea()
+        {
+            return this.Width * this.Height;
+        }
+    }
+
+    internal class AbstractBasic
+    {
+        static void Main(string[] args)
+        {
+            Figure t = new Triangle(10, 30);
+            Console.WriteLine(t.GetArea()); // 150
+
+            Figure s = new Square(10, 30);
+            Console.WriteLine(s.GetArea()); // 300
+        }
+    }
+}
