b46a5452eb243d799c9619948eb7833797e9167a
blue32a <blue32a.jp@gmail.com>
2026-06-21 11:07:34 +0900

    リスト8.19 継承の禁止

---
+++ Chap08/InheritNg.cs
@@ -0,0 +1,29 @@
+﻿namespace SelfCSharp.Chap08.Inherit
+{
+    internal sealed class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+        public string Show()
+        {
+            return $"名前は{this.LastName}{this.FirstName}です。";
+        }
+    }
+
+    // エラー
+    //internal class BusinessPerson : Person { }
+
+    internal class InheritNg
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person()
+            {
+                FirstName = "太郎",
+                LastName = "山田"
+            };
+            Console.WriteLine(p.Show());
+        }
+    }
+}
