0f885c68fb3bfb10d889395dc40ab0fa4550a7e0
blue32a <blue32a.jp@gmail.com>
2026-06-21 17:27:36 +0900

    リスト8.29 関係パターンによるパターンマッチング

---
+++ Chap08/PatternRelation.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap08
+{
+    internal class PatternRelation
+    {
+        static void Main(string[] args)
+        {
+            var point = 45;
+
+            Console.WriteLine(point switch
+            {
+                > 80 => "Excellent!!",
+                > 60 => "Good!",
+                > 40 => "Normal",
+                _ => "Bad...",
+            }); // Normal
+        }
+    }
+}
