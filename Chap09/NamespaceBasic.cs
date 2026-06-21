a1cc3c618cdbfd97e943940322bd778e8fdccfa3
blue32a <blue32a.jp@gmail.com>
2026-06-21 18:46:58 +0900

    リスト9.1,9.2 名前空間の基本

---
+++ Chap09/NamespaceBasic.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfCSharp.Chap09.Ns
+{
+    internal class NamespaceBasic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SelfCSharp.Chap09.Ns.NamespaceBasic");
+        }
+    }
+}
+
+// namespaceブラックのネストで階層構造を表現
+//namespace SelfCSharp
+//{
+//    namespace Chap09
+//    {
+//        namespace Ns
+//        {
+//            internal class NamespaceBasic
+//            {
+//                static void Main(string[] args)
+//                {
+//                    Console.WriteLine("SelfCSharp.Chap09.Ns.NamespaceBasic");
+//                }
+//            }
+//        }
+//    }
+//}
