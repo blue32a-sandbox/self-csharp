a8a1780ec6bc5e861193a03f97cc98dd1a839437
blue32a <blue32a.jp@gmail.com>
2026-06-25 23:31:17 +0900

    リスト9.6 エイリアス修飾子

---
+++ Chap09/NamespaceModifier.cs
@@ -0,0 +1,17 @@
+﻿using Chapter = SelfCSharp.Chap09.Other;
+
+namespace SelfCSharp.Chap09.Ns
+{
+    internal class NamespaceModifier
+    {
+        static void Main(string[] args)
+        {
+            // エイリアスとの衝突
+            //var ci = new Chapter.CultureInfo("ja-JP");
+
+            var ci = new Chapter::CultureInfo("ja-JP");
+        }
+
+        internal class Chapter { }
+    }
+}
