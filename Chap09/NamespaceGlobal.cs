d6cd5eb7207cf1c38ed9ee81c20b07592738e491
blue32a <blue32a.jp@gmail.com>
2026-06-25 23:34:12 +0900

    リスト9.7 グローバル名前空間エイリアス

---
+++ Chap09/NamespaceGlobal.cs
@@ -0,0 +1,21 @@
+﻿internal class Util
+{
+    public static void Run()
+    {
+        Console.WriteLine("Util is running.");
+    }
+}
+
+namespace SelfCSharp.Chap09.Util
+{
+    internal class NamespaceGlobal
+    {
+        static void Main(string[] args)
+        {
+            // グルーバル名前空間のUtilが重複している
+            //Util.Run();
+
+            global::Util.Run();
+        }
+    }
+}
