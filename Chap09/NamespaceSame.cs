29586a155579502d6b963cb7cad693254001f373
blue32a <blue32a.jp@gmail.com>
2026-06-25 23:21:22 +0900

    リスト9.3 完全修飾名による参照

---
+++ Chap09/NamespaceSame.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using SelfCSharp.Chap09.Other;
+
+namespace SelfCSharp.Chap09.Ns
+{
+    internal class NamespaceSame
+    {
+        static void Main(string[] args)
+        {
+            // あいまいな参照
+            //var ci = new CultureInfo("ja-JP");
+
+            // 完全修飾名による参照
+            var ci = new System.Globalization.CultureInfo("ja-JP");
+        }
+    }
+}
