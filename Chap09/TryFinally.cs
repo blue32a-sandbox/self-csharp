678cd17e945b4a41b1a31165678a36bb2414c196
blue32a <blue32a.jp@gmail.com>
2026-06-28 16:01:49 +0900

    リスト9.12 例外の有無に関わらず実行されるfinalyブロック

---
+++ Chap09/TryFinally.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SelfCSharp.Chap09
+{
+    internal class TryFinally
+    {
+        static void Main(string[] args)
+        {
+            StreamReader? sr = null;
+            try
+            {
+                sr = new StreamReader(@"C:\noting.dat");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+        }
+    }
+}
