73cc98b6a8f5da4a2d1631d6d6f9707ec1896c74
blue32a <blue32a.jp@gmail.com>
2026-06-28 18:11:03 +0900

    リスト9.22 オーバーフローのチェック

---
+++ Chap09/Overflow.cs
@@ -0,0 +1,11 @@
+﻿namespace SelfCSharp.Chap09
+{
+    internal class Overflow
+    {
+        static void Main(string[] args)
+        {
+            var i = int.MaxValue;
+            Console.WriteLine(++i);
+        }
+    }
+}
