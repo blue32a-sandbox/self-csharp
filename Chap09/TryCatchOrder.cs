edaf142d25a5ed8c17468fbe437120c138f8aa2f
blue32a <blue32a.jp@gmail.com>
2026-06-28 16:51:05 +0900

    リスト9.13 catchブロックの記述順序

---
+++ Chap09/TryCatchOrder.cs
@@ -0,0 +1,36 @@
+﻿namespace SelfCSharp.Chap09
+{
+    internal class TryCatchOrder
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var sr = new StreamReader(@"C:\nothing.dat"))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            // 先頭のExceptionクラスがすべての例外を補足してしまうためエラーになる
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("ArgumentNullException");
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("FileNotFoundException");
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("ArgumentException");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
