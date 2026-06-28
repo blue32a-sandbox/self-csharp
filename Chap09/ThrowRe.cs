1cc28879c353f2f8d7eb43a14d7c93526bc12ad0
blue32a <blue32a.jp@gmail.com>
2026-06-28 18:03:57 +0900

    リスト9.19 例外を再スロー

---
+++ Chap09/ThrowRe.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap09
+{
+    internal class ThrowRe
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var sr = new StreamReader(@"C:\noting.dat"))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("FileNotFoundExceptionをキャッチして再スロー");
+                throw;
+
+                // スタックトレースを上書きしてしまう
+                //throw ex;
+            }
+        }
+    }
+}
