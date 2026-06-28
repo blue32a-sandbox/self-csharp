d892c1ce54b5ccdcd1c45c6751b04b8cbcfd8c5f
blue32a <blue32a.jp@gmail.com>
2026-06-28 17:57:07 +0900

    リスト9.18 throw式

---
+++ Chap09/ThrowExp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SelfCSharp.Chap09
+{
+    internal class ThrowExp
+    {
+        static void Main(string[] args)
+        {
+            var i = -10;
+            Console.WriteLine(i > 0 ? i :
+                throw new Exception("iは正数でなければいけません。"));
+        }
+    }
+}
