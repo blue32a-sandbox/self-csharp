05257ea868defe20ebeb78b24e4d6ddc1599e2c4
blue32a <blue32a.jp@gmail.com>
2026-06-29 23:56:47 +0900

    リスト9.32 列挙値を文字列として取得する

---
+++ Chap09/EnumString.cs
@@ -0,0 +1,14 @@
+﻿namespace SelfCSharp.Chap09
+{
+    internal class EnumString
+    {
+        static void Main(string[] args)
+        {
+            var s = Season.Spring;
+            Console.WriteLine(s); // Spring
+            Console.WriteLine(s.ToString()); // Spring
+            Console.WriteLine(s.ToString("D")); // 1
+            Console.WriteLine(s.ToString("X")); // 0000000000000001
+        }
+    }
+}
