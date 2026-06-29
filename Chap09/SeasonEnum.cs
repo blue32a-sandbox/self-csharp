5b193e651ff61b0d4828b7d36eb3ee6b0ba60950
blue32a <blue32a.jp@gmail.com>
2026-06-29 22:42:31 +0900

    リスト9.28 列挙型の基本

---
+++ Chap09/SeasonEnum.cs
@@ -0,0 +1,39 @@
+﻿namespace SelfCSharp.Chap09
+{
+    enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
+    internal class SeasonEnum
+    {
+        public void ProcessSeason(Season season)
+        {
+            Console.WriteLine(
+                season switch
+                {
+                    Season.Spring => "春です。",
+                    Season.Summer => "夏です。",
+                    Season.Autumn => "秋です。",
+                    Season.Winter => "冬です。",
+                    _ => "不明な季節です。"
+                }
+             );
+        }
+    }
+
+    internal class SeasonEnumClient
+    {
+        static void Main(string[] args)
+        {
+            var se = new SeasonEnum();
+            se.ProcessSeason(Season.Spring);
+
+            // コンパイルエラー
+            //se.ProcessSeason(4);
+        }
+    }
+}
