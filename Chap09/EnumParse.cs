0a83bc8c5fe11617b420183ff16efb8a82b3531f
blue32a <blue32a.jp@gmail.com>
2026-06-30 00:03:28 +0900

    リスト9.33,9.34 文字列を列挙型の値に変換するParse / TryParse メソッド

---
+++ Chap09/EnumParse.cs
@@ -0,0 +1,16 @@
+﻿namespace SelfCSharp.Chap09
+{
+    internal class EnumParse
+    {
+        static void Main(string[] args)
+        {
+            var str = (Season)Enum.Parse(typeof(Season), "Summer");
+            var num = (Season)Enum.Parse(typeof(Season), "2");
+            Console.WriteLine($"{str} - {str.GetType()}"); // Summer - SelfCSharp.Chap09.Season
+            Console.WriteLine($"{num} - {num.GetType()}"); // Summer - SelfCSharp.Chap09.Season
+
+            var success = Enum.TryParse("Summer", out Season s);
+            Console.WriteLine(success ? $"{s}" : "変換失敗"); // Summer
+        }
+    }
+}
