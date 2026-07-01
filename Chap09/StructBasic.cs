3554f3e09155c3adde4a5f5fa87e5621e201eb62
blue32a <blue32a.jp@gmail.com>
2026-07-01 23:13:57 +0900

    リスト9.37 構造体

---
+++ Chap09/StructBasic.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap09
+{
+    struct Coordinates
+    {
+        public double Latitude;
+        public double Longitude;
+
+        public override string ToString()
+        {
+            return $"緯度: {this.Latitude}／経度: {this.Longitude}";
+        }
+    }
+
+    internal class StructBasic
+    {
+        static void Main(string[] args)
+        {
+            var c = new Coordinates();
+            c.Latitude = 35.681167;
+            c.Longitude = 139.767052;
+            Console.WriteLine(c); // 緯度: 35.681167／経度: 139.767052
+        }
+    }
+}
