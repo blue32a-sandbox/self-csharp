9458a017f048b65a422de789dd8024d5c8755f33
blue32a <blue32a.jp@gmail.com>
2026-07-05 16:31:53 +0900

    リスト9.61 Objectクラス ToStringメソッド

---
+++ Chap09/ObjString.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfCSharp.Chap09.ObjectBasic
+{
+    internal class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return $"Person: {this.LastName} {this.FirstName}";
+        }
+    }
+
+    internal class ObjString
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("太郎", "山田");
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
