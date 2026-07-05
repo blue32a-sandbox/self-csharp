e0848dcc93f7d009719a6e2b4836957765c67f58
blue32a <blue32a.jp@gmail.com>
2026-07-05 13:06:34 +0900

    リスト 9.42,9.43 レコード型のプロパティ定義

---
+++ Chap09/RecordProp.cs
@@ -0,0 +1,25 @@
+﻿namespace SelfCSharp.Chap09.RecordProp
+{
+    public record Person
+    {
+        // プライマリコンストラクターを使用しない従来の構文
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+    }
+
+    internal class RecordProp
+    {
+        // オブジェクト初期化を使用して初期化する
+        static void Main(string[] args)
+        {
+            var p = new Person()
+            {
+                FirstName = "太郎",
+                LastName = "山田",
+                Age = 10
+            };
+            Console.WriteLine(p); // Person { FirstName = 太郎, LastName = 山田, Age = 10 }
+        }
+    }
+}
