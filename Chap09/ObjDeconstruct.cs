dabaebfc114ef3ff4c5d596ba03f5906c8d748e1
blue32a <blue32a.jp@gmail.com>
2026-07-05 17:00:54 +0900

    リスト9.66 Objectクラス Deconstructメソッド

---
+++ Chap09/ObjDeconstruct.cs
@@ -0,0 +1,33 @@
+﻿namespace SelfCSharp.Chap09
+{
+    internal class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public void Deconstruct(out string firstName, out string LastName)
+        {
+            firstName = this.FirstName;
+            LastName = this.LastName;
+        }
+    }
+
+    internal class ObjDeconstruct
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("太郎", "山田", 20);
+            var (fn, ln) = p;
+            Console.WriteLine(fn); // 太郎
+            Console.WriteLine(ln); // 山田
+        }
+    }
+}
