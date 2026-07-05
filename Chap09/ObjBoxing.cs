df8e8b06983e93f9b531f3672dceadea163b3696
blue32a <blue32a.jp@gmail.com>
2026-07-05 17:06:48 +0900

    リスト9.68 ボクシングとアンボクシング

---
+++ Chap09/ObjBoxing.cs
@@ -0,0 +1,16 @@
+﻿namespace SelfCSharp.Chap09
+{
+    internal class ObjBoxing
+    {
+        static void Main(string[] args)
+        {
+            int data = 100;
+            Console.WriteLine(data.ToString()); // Objectクラスのメンバーを利用できる
+            object obj = data; // object型にも代入できる（ボックス化／ボクシング）
+            Console.WriteLine(obj);
+
+            int i = (int)obj; // 元の型に戻す（ボックス化解除／アンボクシング）
+            Console.WriteLine(i);
+        }
+    }
+}
