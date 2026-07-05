daef5823b4eda7f83e92468db19ac58ead0c872b
blue32a <blue32a.jp@gmail.com>
2026-07-05 12:53:57 +0900

    リスト9.41 レコード型

---
+++ Chap09/RecordBasic.cs
@@ -0,0 +1,20 @@
+﻿namespace SelfCSharp.Chap09.Record
+{
+    // プライマリコンストラクター
+    public record Person(string FirstName, string LastName, int Age);
+
+    internal class RecordBasic
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person("太郎", "山田", 10);
+            var p2 = new Person("太郎", "山田", 10);
+
+            Console.WriteLine(p1); // Person { FirstName = 太郎, LastName = 山田, Age = 10 }
+
+            Console.WriteLine(p1 == p2); // True
+            (string fname, string lname, int age) = p1;
+            Console.WriteLine(fname); // 太郎
+        }
+    }
+}
