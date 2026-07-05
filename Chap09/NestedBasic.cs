568a9eed0dd603aba06efbccafe56c0d4dc3e625
blue32a <blue32a.jp@gmail.com>
2026-07-05 13:43:01 +0900

    リスト9.48 入れ子クラス

---
+++ Chap09/NestedBasic.cs
@@ -0,0 +1,31 @@
+﻿namespace SelfCSharp.Chap09.Nested
+{
+    internal class MyClass
+    {
+        class MyHelper
+        {
+            public void Show()
+            {
+                Console.WriteLine("Nested Class is running!");
+            }
+        }
+
+        public void Run()
+        {
+            var helper = new MyHelper();
+            helper.Show();
+        }
+    }
+
+    internal class NestedBasic
+    {
+        static void Main(string[] args)
+        {
+            var c = new MyClass();
+            c.Run();
+
+            // エラー
+            //var h = new MyClass().MyHelper();
+        }
+    }
+}
