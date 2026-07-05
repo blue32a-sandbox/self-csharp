ef38bc66734e2d3af3f6e5e311534c3f57bb4cb4
blue32a <blue32a.jp@gmail.com>
2026-07-05 17:28:24 +0900

    リスト9.72,9.73 インクリメント演算子のオーバーロード

---
+++ Chap09/OpeIncrement.cs
@@ -0,0 +1,38 @@
+﻿namespace SelfCSharp.Chap09.Operator2
+{
+    internal class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static Coordinate operator ++(Coordinate c)
+        {
+            return new Coordinate
+            {
+                X = c.X + 1,
+                Y = c.Y + 1
+            };
+
+            // 引数をそのまま演算してはいけない
+            //c.X++;
+            //c.Y++;
+            //return c;
+        }
+    }
+
+    internal class OpeIncrement
+    {
+        static void Main(string[] args)
+        {
+            var a = new Coordinate() { X = 10, Y = 20 };
+            var b = ++a;
+            Console.WriteLine($"({a.X},{a.Y})"); // (11,21)
+            Console.WriteLine($"({b.X},{b.Y})"); // (11,21)
+
+            var m = new Coordinate() { X = 10, Y = 20 };
+            var n = m++;
+            Console.WriteLine($"({m.X},{m.Y})"); // (11,21)
+            Console.WriteLine($"({n.X},{n.Y})"); // (10,20)
+        }
+    }
+}
