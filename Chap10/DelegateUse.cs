feb38c25cf18249a4557188dc96d1174f8847ae8
blue32a <blue32a.jp@gmail.com>
2026-07-06 23:09:34 +0900

    リスト10.2,10.3 デリゲートを使わない例と使った例

---
+++ Chap10/DelegateUse.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfCSharp.Chap10
+{
+    delegate void OutputProcess(string str);
+
+    internal class DelegateUse
+    {
+        void ArrayWalk(string[] data, OutputProcess output)
+        {
+            foreach (var value in data)
+            {
+                output(value);
+            }
+        }
+
+        static void AddQuote(string data)
+        {
+            Console.WriteLine($"[{data}]");
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] { "あかまきがみ", "あおまきがみ", "きまきがみ" };
+            var du = new DelegateUse();
+            OutputProcess proc = AddQuote;
+            du.ArrayWalk(data, proc);
+        }
+    }
+}
