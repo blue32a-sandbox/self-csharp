6a48770eb5cd39cb3aad3c48ed0cb4a12dbedf8f
blue32a <blue32a.jp@gmail.com>
2026-07-06 22:58:06 +0900

    リスト10.1 デリゲート

---
+++ Chap10/DelegateBasic.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap10
+{
+    delegate void Process(string str);
+
+    internal class DelegateBasic
+    {
+        static void Run(string s)
+        {
+            Console.WriteLine($"{s}走ります。");
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Process(Run);
+            p("ちょこちょこ"); // ちょこちょこ走ります。
+        }
+    }
+}
