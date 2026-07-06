8346ab17336669125cce04bac128e4909b45b1ee
blue32a <blue32a.jp@gmail.com>
2026-07-06 23:18:42 +0900

    リスト10.4 デリゲートによるメソッドの差し替え

---
+++ Chap10/DelegateUseCounter.cs
@@ -0,0 +1,33 @@
+﻿namespace SelfCSharp.Chap10.Delegate
+{
+    internal class DelegateUseCounter
+    {
+        void ArrayWalk(string[] data, OutputProcess output)
+        {
+            foreach (var value in data)
+            {
+                output(value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] { "あかまきがみ", "あおまきがみ", "きまきがみ" };
+            
+            var du = new DelegateUseCounter();
+            var c = new Counter();
+            du.ArrayWalk(data, c.AddLength);
+            Console.WriteLine(c.Result); // 17
+        }
+    }
+
+    internal class Counter
+    {
+        public int Result { get; private set; }
+
+        public void AddLength(string value)
+        {
+            Result += value.Length;
+        }
+    }
+}
