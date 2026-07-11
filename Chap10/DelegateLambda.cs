7e6473105713e50aab4d78564a382c6d5e04cd23
blue32a <blue32a.jp@gmail.com>
2026-07-11 09:33:12 +0900

    リスト10.9 ラムダ式

---
+++ Chap10/DelegateLambda.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class DelegateLambda
+    {
+        void ArrayWalk(string[] data, Func<string, string> output)
+        {
+            foreach (var value in data)
+            {
+                Console.WriteLine(output(value));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] { "あかまきがみ", "あおまきがみ", "きまきがみ" };
+
+            var dl = new DelegateLambda();
+            dl.ArrayWalk(data, (string d) =>
+            {
+                return $"[{d}]";
+            });
+        }
+    }
+}
