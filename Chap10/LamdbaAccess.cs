46b803453c1118cb604b6f5932664ebaddf17906
blue32a <blue32a.jp@gmail.com>
2026-07-11 22:57:21 +0900

    リスト10.10 ラムダ式の上位スコープへのアクセス

---
+++ Chap10/LamdbaAccess.cs
@@ -0,0 +1,12 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LamdbaAccess
+    {
+        static void Main(string[] args)
+        {
+            var msg = "鈴木次郎";
+            var show = () => { Console.WriteLine(msg); };
+            show(); // 鈴木次郎
+        }
+    }
+}
