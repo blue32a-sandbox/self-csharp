cdaf6a39875cce7c5367a813ddc4132527c09453
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:17:05 +0900

    リスト10.28 データの検索条件を指定する where句/Whereメソッド > 候補値検索

---
+++ Chap10/LinqIn.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqIn
+    {
+        static void Main(string[] args)
+        {
+            var bs = AppTables.Books
+                     .Where(b => new int[] { 3, 6 }.Contains(b.Published.Month))
+                     .Select(b => b);
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
