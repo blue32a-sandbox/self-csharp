1d0439b93d2813e612dcdd63613795f93c6be80f
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:14:58 +0900

    リスト10.27 データの検索条件を指定する where句/Whereメソッド > 前方一致/後方一致検索

---
+++ Chap10/LinqStartsWith.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqStartsWith
+    {
+        static void Main(string[] args)
+        {
+            var bs = AppTables.Books
+                     .Where(b => b.Title.StartsWith("Android"))
+                     .Select(b => b);
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
