3d6eb31dcd66e0a8a458f004a44f5cb72ec1e039
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:50:20 +0900

    リスト10.39,10.40 データをグループ化する group句/GroupByメソッド

---
+++ Chap10/LinqGroup.cs
@@ -0,0 +1,22 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqGroup
+    {
+        static void Main(string[] args)
+        {
+            //var bs = from b in AppTables.Books
+            //         group b by b.Publisher;
+            var bs = AppTables.Books
+                     .GroupBy(b => b.Publisher);
+            
+            foreach (var b in bs)
+            {
+                Console.WriteLine($"[{b.Key}]");
+                foreach (var t in b)
+                {
+                    Console.WriteLine($"{t.Title} ({t.Price}円)");
+                }
+            }
+        }
+    }
+}
---
+++ Chap10/LinqGroup2.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqGroup2
+    {
+        static void Main(string[] args)
+        {
+            //var bs = from b in AppTables.Books
+            //         group new { Title = b.Title, Price = b.Price }
+            //         by b.Publisher;
+            var bs = AppTables.Books
+                     .GroupBy(b => b.Publisher,
+                        b => new { Title = b.Title, Price = b.Price });
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine($"[{b.Key}]");
+                foreach (var t in b)
+                {
+                    Console.WriteLine($"{t.Title} ({t.Price}円)");
+                }
+            }
+        }
+    }
+}
