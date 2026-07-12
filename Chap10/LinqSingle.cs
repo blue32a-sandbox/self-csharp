ed95c6dc3cbe57a0c93c9a06cc1c5949d166c0b1
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:25:35 +0900

    リスト10.30,10.31 単一の要素を取得する Singleメソッド

---
+++ Chap10/LinqSingle.cs
@@ -0,0 +1,15 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqSingle
+    {
+        static void Main(string[] args)
+        {
+            var b = AppTables.Books
+                     .Single(b => b.Isbn == "978-4-7981-6849-4");
+            //var b = AppTables.Books
+            //         .SingleOrDefault(b => b.Isbn == "xxx-x-xxxx-xxxx-x");
+
+            Console.WriteLine(b);
+        }
+    }
+}
