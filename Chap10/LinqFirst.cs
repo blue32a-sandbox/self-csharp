72dd444977991c59f8548f8b83f34f7deeaea505
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:43:48 +0900

    リスト10.37,10.38 先頭のデータを取得する Firstメソッド

---
+++ Chap10/LinqFirst.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqFirst
+    {
+        static void Main(string[] args)
+        {
+            var b = AppTables.Books
+                    .OrderBy(b => b.Price)
+                    .First();
+            //var b = AppTables.Books
+            //        .Where(b => b.Price > 10000)
+            //        .OrderBy(b => b.Price)
+            //        .FirstOrDefault();
+
+            Console.WriteLine(b);
+        }
+    }
+}
