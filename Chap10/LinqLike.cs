149cea9501421ff9348e0be8a2f767ef751b49eb
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:12:00 +0900

    リスト10.26 データの検索条件を指定する where句/Whereメソッド > 部分一致検索

---
+++ Chap10/LinqLike.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqLike
+    {
+        static void Main(string[] args)
+        {
+            var bs = AppTables.Books
+                     .Where(b => b.Title.Contains("Android"))
+                     .Select(b => b);
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
