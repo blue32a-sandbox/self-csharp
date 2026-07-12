232b61c53a8fad89e0df42d8a982c80d9db4a507
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:08:00 +0900

    リスト10.24,10,25 LINQの遅延実行

---
+++ Chap10/LinqQueryDelay.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqQueryDelay
+    {
+        static void Main(string[] args)
+        {
+            var bs = from b in AppTables.Books select b.Title;
+
+            // ToArray()を利用すると、遅延実行がその場で確定する
+            //var bs = (from b in AppTables.Books select b.Title).ToArray();
+
+            AppTables.Books.ElementAt(0).Title = "独学できるPHP";
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
