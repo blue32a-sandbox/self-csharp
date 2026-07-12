3284076c3071203d29d8ddea636336aeffb862b7
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:29:56 +0900

    リスト10.32,10.33 データを並べ替える orderby句/OrderByメソッド

---
+++ Chap10/LinqOrder.cs
@@ -0,0 +1,21 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqOrder
+    {
+        static void Main(string[] args)
+        {
+            //var bs = from b in AppTables.Books
+            //         orderby b.Price descending, b.Published
+            //         select b;
+            var bs = AppTables.Books
+                     .OrderByDescending(b => b.Price)
+                     .ThenBy(b => b.Published)
+                     .Select(b => b);
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
