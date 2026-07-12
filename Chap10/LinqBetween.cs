c9a8b76b1409f1b71f1bcf8d251d44fb11082cfe
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:18:52 +0900

    リスト10.29 データの検索条件を指定する where句/Whereメソッド > 範囲検索

---
+++ Chap10/LinqBetween.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqBetween
+    {
+        static void Main(string[] args)
+        {
+            var bs = AppTables.Books
+                     .Where(b => 2000 <= b.Price && b.Price <= 3500)
+                     .Select(b => b);
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
