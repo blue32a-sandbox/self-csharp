e47665a9d9c3018b2ddcdbc5aeba757d88717654
blue32a <blue32a.jp@gmail.com>
2026-07-12 11:10:39 +0900

    リスト10.13 ラムダ式を伴うListクラスのメソッド > ForEach

---
+++ Chap10/ListForeach.cs
@@ -0,0 +1,11 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class ListForeach
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 3, 6, 9 };
+            list.ForEach(v => Console.WriteLine(v * v)); // 1, 9, 36, 81
+        }
+    }
+}
