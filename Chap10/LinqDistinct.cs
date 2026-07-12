5e9247fbc408418c0bba62302055ba838cfb28a6
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:36:49 +0900

    リスト10.35 データの重複を除去する Distinctメソッド

---
+++ Chap10/LinqDistinct.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqDistinct
+    {
+        static void Main(string[] args)
+        {
+            var bs = AppTables.Books
+                     .Select(b => b.Publisher)
+                     .Distinct();
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
