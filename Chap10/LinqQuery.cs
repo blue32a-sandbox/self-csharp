15394b6f1620ec9803953fb22968b9421223f212
blue32a <blue32a.jp@gmail.com>
2026-07-12 11:59:51 +0900

    リスト10.22 LINQ クエリ構文

---
+++ Chap10/LinqQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqQuery
+    {
+        static void Main(string[] args)
+        {
+            var bs = from b in AppTables.Books
+                     where b.Price < 3000
+                     select b.Title;
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
