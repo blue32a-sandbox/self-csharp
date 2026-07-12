5f378b802cdd3c782d86d0ee017849da378bf4cf
blue32a <blue32a.jp@gmail.com>
2026-07-12 13:05:07 +0900

    リスト10.42,10.43 グループ化した結果を絞り込む into句

---
+++ Chap10/LinqInto.cs
@@ -0,0 +1,30 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqInto
+    {
+        static void Main(string[] args)
+        {
+            //var bs = from b in AppTables.Books
+            //         group b by b.Publisher into pubs
+            //         where pubs.Average(b => b.Price) >= 3500
+            //         select new
+            //         {
+            //             Published = pubs.Key,
+            //             AveragePrice = pubs.Average(b => b.Price)
+            //         };
+            var bs = AppTables.Books
+                     .GroupBy(b => b.Publisher)
+                     .Where(pubs => pubs.Average(b => b.Price) >= 3500)
+                     .Select(pubs => new
+                     {
+                         Published = pubs.Key,
+                         AveragePrice = pubs.Average(b => b.Price)
+                     });
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine($"{b.Published} {b.AveragePrice}円");
+            }
+        }
+    }
+}
---
+++ Chap10/LinqInto2.cs
@@ -0,0 +1,34 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqInto2
+    {
+        static void Main(string[] args)
+        {
+            //var bs = from b in AppTables.Books
+            //         group b by new
+            //         {
+            //             PublishYear = b.Published.Year,
+            //             PublishMonth = b.Published.Month
+            //         } into pubs
+            //         orderby pubs.Key.PublishYear, pubs.Key.PublishMonth
+            //         select pubs;
+            var bs = AppTables.Books
+                     .GroupBy(b => new
+                     {
+                         PublishYear = b.Published.Year,
+                         PublishMonth = b.Published.Month
+                     })
+                     .OrderBy(pubs => pubs.Key.PublishYear)
+                     .ThenBy(pubs => pubs.Key.PublishMonth);
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine($"[{b.Key.PublishYear}年-{b.Key.PublishMonth}月]");
+                foreach (var t in b)
+                {
+                    Console.WriteLine(t.Title);
+                }
+            }
+        }
+    }
+}
