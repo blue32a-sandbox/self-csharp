7ce2ac5a0ee3d654954c8d2c2caae33b1b2ab337
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:54:11 +0900

    リスト10.41 複数のキーでグループ化

---
+++ Chap10/LinqGroupMulti.cs
@@ -0,0 +1,30 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqGroupMulti
+    {
+        static void Main(string[] args)
+        {
+            //var bs = from b in AppTables.Books
+            //         group b by new
+            //         {
+            //             Publisher = b.Publisher,
+            //             PublishYear = b.Published.Year
+            //         };
+            var bs = AppTables.Books
+                     .GroupBy(b => new
+                     {
+                         Publisher = b.Publisher,
+                         PublishYear = b.Published.Year
+                     });
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine($"[{b.Key.Publisher}-{b.Key.PublishYear}]");
+                foreach (var t in b)
+                {
+                    Console.WriteLine($"{t.Title} ({t.Price}円)");
+                }
+            }
+        }
+    }
+}
