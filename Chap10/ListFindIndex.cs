0e09a61eab984f1bc04802285011b75e50e368dd
blue32a <blue32a.jp@gmail.com>
2026-07-12 11:19:25 +0900

    リスト10.17 ラムダ式を伴うListクラスのメソッド > FindIndex/FIndLastIndex

---
+++ Chap10/ListFindIndex.cs
@@ -0,0 +1,13 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class ListFindIndex
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, -15, 30, 60, -50, 40 };
+            Console.WriteLine(list.FindIndex(v => v < 0)); // 1
+            Console.WriteLine(list.FindLastIndex(v => v < 0)); // 4
+            Console.WriteLine(list.FindIndex(2, 3, v => v < 0)); // 4
+        }
+    }
+}
