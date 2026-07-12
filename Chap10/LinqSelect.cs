a9793af31c7d220605d57fd3ff0c79bb8d3d87d8
blue32a <blue32a.jp@gmail.com>
2026-07-12 12:34:08 +0900

    リスト10.34 特定のプロパティだけを取り出す select句/Slectメソッド

---
+++ Chap10/LinqSelect.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfCSharp.Chap10
+{
+    internal class LinqSelect
+    {
+        static void Main(string[] args)
+        {
+            //var bs = from b in AppTables.Books
+            //         select new
+            //         {
+            //             ShortTitle = b.Title.Substring(0, 5),
+            //             DiscountPrice = b.Price * 0.9,
+            //             Released = (b.Published <= DateTime.Now ? "発売中" : "発売予定")
+            //         };
+            var bs = AppTables.Books
+                     .Select(b => new
+                     {
+                         ShortTitle = b.Title.Substring(0, 5),
+                         DiscountPrice = b.Price * 0.9,
+                         Released = (b.Published <= DateTime.Now ? "発売中" : "発売予定")
+                     });
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
