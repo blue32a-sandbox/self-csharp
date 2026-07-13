ada5ee3debf85689ab6ac29eb94109adc0308aac
blue32a <blue32a.jp@gmail.com>
2026-07-13 23:20:04 +0900

    リスト11.3,11.4 lockブロックによる排他制御

---
+++ Chap11/LockBasic.cs
@@ -0,0 +1,35 @@
+﻿namespace SelfCSharp.Chap11
+{
+    internal class LockBasic
+    {
+        public int Count { get; private set; } = 0;
+        object lockobj = new object();
+
+        static void Main(string[] args)
+        {
+            const int TaskNum = 500000;
+            var ts = new Task[TaskNum];
+            var tb = new LockBasic();
+
+            for (var i = 0; i < TaskNum; i++)
+            {
+                ts[i] = Task.Run(() => tb.Increment());
+            }
+
+            for (var i = 0; i < TaskNum; i++)
+            {
+                ts[i].Wait();
+            }
+
+            Console.WriteLine(tb.Count); // 500000
+        }
+
+        void Increment()
+        {
+            lock (lockobj)
+            {
+                this.Count++;
+            }
+        }
+    }
+}
