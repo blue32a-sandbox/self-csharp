5847b3878700e81112cd38c401532cfcd0c98ec8
blue32a <blue32a.jp@gmail.com>
2026-07-13 23:07:56 +0900

    リスト11.2 タスクの生成と実行

---
+++ Chap11/ThreadBasic.cs
@@ -0,0 +1,25 @@
+﻿namespace SelfCSharp.Chap11
+{
+    internal class ThreadBasic
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = Task.Run(() => Count(1));
+            Task t2 = Task.Run(() => Count(2));
+            Task t3 = Task.Run(() => Count(3));
+
+            t1.Wait();
+            t2.Wait();
+            t3.Wait();
+            Console.WriteLine("すべての処理が終了しました。");
+        }
+
+        static void Count(object? n)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"Task{n}: {i}");
+            }
+        }
+    }
+}
