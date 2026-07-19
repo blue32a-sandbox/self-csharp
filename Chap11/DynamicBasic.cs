49ba3a611e9c09d32f782730281ea3ab0880dd20
blue32a <blue32a.jp@gmail.com>
2026-07-19 16:01:43 +0900

    リスト11.19 dynamic型の基本

---
+++ Chap11/DynamicBasic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SelfCSharp.Chap11
+{
+    internal class DynamicBasic
+    {
+        static void Main(string[] args)
+        {
+            dynamic d = 10; // 整数
+            d = "ほげ"; // 文字列
+            d.Hoge(); // string型にHogeメソッドはないのでエラー
+        }
+    }
+}
