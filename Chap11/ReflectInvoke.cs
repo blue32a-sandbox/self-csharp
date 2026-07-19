b3569bd71b344478af7a5fcf1adf876f50ff55c1
blue32a <blue32a.jp@gmail.com>
2026-07-19 15:54:23 +0900

    リスト11.18 リフレクションでメソッド実行

---
+++ Chap11/ReflectInvoke.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfCSharp.Chap11
+{
+    internal class ReflectInvoke
+    {
+        static void Main(string[] args)
+        {
+            // FileInfoクラスをインスタンス化
+            var fi = typeof(FileInfo);
+            var cf = fi.GetConstructor(new Type[] { typeof(string) });
+            var obj = cf?.Invoke(new[] { @"c:\data\result.txt" });
+
+            // CopyToメソッドを呼び出す
+            var m = fi.GetMethod("CopyTo", new Type[] { typeof (string) });
+            m?.Invoke(obj, new object[] { @"c:\data\copy.txt" });
+        }
+    }
+}
