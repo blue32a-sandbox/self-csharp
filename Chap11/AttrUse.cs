9385dd4b84d2a90ec243d562d5d7306f68cd8a89
blue32a <blue32a.jp@gmail.com>
2026-07-19 11:03:14 +0900

    リスト11.15 属性の利用

---
+++ Chap11/AttrUse.cs
@@ -0,0 +1,18 @@
+﻿namespace SelfCSharp.Chap11
+{
+    [Version("1.0.0", Beta = true)]
+    internal class AttrUse
+    {
+        static void Main(string[] args)
+        {
+            var t = typeof(AttrUse);
+            var attr = Attribute.GetCustomAttribute(
+                t, typeof(VersionAttribute)) as VersionAttribute;
+            if (attr is not null)
+            {
+                Console.WriteLine(attr.Number); // 1.0.0
+                Console.WriteLine("Β版で" + (attr.Beta ? "す" : "はありません")); // Β版です
+            }
+        }
+    }
+}
