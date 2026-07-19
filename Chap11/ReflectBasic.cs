266a6dd86049b32c31ed9a216ca4df5aa1643e23
blue32a <blue32a.jp@gmail.com>
2026-07-19 15:29:16 +0900

    リスト11.16 リフレクションによるオブジェクトの生成

---
+++ Chap11/ReflectBasic.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap11
+{
+    internal class ReflectBasic
+    {
+        static void Main(string[] args)
+        {
+            // 引数なしのコンストラクター
+            var tp = typeof(Person);
+            var op = Activator.CreateInstance(tp);
+            Console.WriteLine(op); // SelfCSharp.Chap11.Person
+
+            // 引数ありのコンストラクター
+            var tf = typeof(FileInfo);
+            var cf = tf.GetConstructor(new[] { typeof(string) });
+            var of = cf?.Invoke(new[] { @"c:\data\result.txt" });
+            Console.WriteLine(of); // c:\data\result.txt
+        }
+    }
+}
