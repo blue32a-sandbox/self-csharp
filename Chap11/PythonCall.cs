e8a7d01f115479fc8a5909d1db998686aaaefc6a
blue32a <blue32a.jp@gmail.com>
2026-07-19 16:16:35 +0900

    リスト11.20,11.21 動的型付け言語との連携

---
+++ Chap11/PythonCall.cs
@@ -0,0 +1,15 @@
+﻿using IronPython.Hosting;
+
+namespace SelfCSharp.Chap11
+{
+    internal class PythonCall
+    {
+        static void Main(string[] args)
+        {
+            var py = Python.CreateRuntime();
+            dynamic script = py.UseFile("Chap11/myClass.py");
+            dynamic clazz = script.MyClass();
+            Console.WriteLine(clazz.greet("山田"));
+        }
+    }
+}
